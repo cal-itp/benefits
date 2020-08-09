f3bd5d598a8daf70d8f6387db5ccb89dac99f400
Kegan Maher <kegan@compiler.la>
2020-08-09 05:42:26 +0000

    helper middleware for debug info

--- eligibility_verification/core/middleware.py
+++ eligibility_verification/core/middleware.py
@@ -1,7 +1,25 @@
 """
 The core application: middleware definitions for request/response cycle.
 """
-from eligibility_verification.settings import TRANSIT_AGENCY
+from eligibility_verification.settings import DEBUG, TRANSIT_AGENCY
+
+
+class DebugMiddleware():
+    """Middleware to configure the debug information for each request."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.key = "debug"
+
+    def __call__(self, request):
+        request.session[self.key] = DEBUG
+        return self.get_response(request)
+
+    def process_template_response(self, request, response):
+        if response.context_data is None:
+            response.context_data = {}
+        response.context_data[self.key] = DEBUG
+        return response
 
 
 class TransitAgencyMiddleware():
--- eligibility_verification/settings.py
+++ eligibility_verification/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 
+    "eligibility_verification.core.middleware.DebugMiddleware",
     "eligibility_verification.core.middleware.TransitAgencyMiddleware"
 ]
 
