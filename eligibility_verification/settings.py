e7d9b92dbe8b2f7158a890c6525baf75d5f2964a
Kegan Maher <kegan@compiler.la>
2020-11-25 20:36:45 +0000

    move static files to project root

--- eligibility_verification/settings.py
+++ eligibility_verification/settings.py
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "eligibility_verification", "static")
+]
