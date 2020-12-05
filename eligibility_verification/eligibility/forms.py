dbda15f94d239c684c0b40426f7d0b819f931314
Kegan Maher <kegan@compiler.la>
2020-12-05 07:34:56 +0000

    display form field errors from the API server

--- eligibility_verification/eligibility/forms.py
+++ eligibility_verification/eligibility/forms.py
@@ -24,3 +24,26 @@ class EligibilityVerificationForm(forms.Form):
 
     submit_value = "Check status"
     submitting_value = "Checking"
+
+    _error_messages = {
+        "invalid": "Check your input. The format looks wrong.",
+        "missing": "This field is required."
+    }
+
+    def add_api_errors(self, form_errors):
+        """Handle errors passed back from API server related to submitted form values."""
+
+        for form_error in form_errors:
+            field_errors = [
+                (field, code)
+                for field, code in form_error.items()
+                if field in self.fields
+            ]
+
+            validation_errors = {
+                field: forms.ValidationError(self._error_messages.get(code, "Error"), code=code)
+                for (field, code) in field_errors
+            }
+
+            for (field, err) in validation_errors.items():
+                self.add_error(field, err)
