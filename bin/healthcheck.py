52458cacface0a528c54f4e5ebe0d58b0ddfae55
Kegan Maher <kegan@compiler.la>
2021-02-19 10:46:19 -0800

    simple healthcheck endpoint, fix exit codes

--- bin/healthcheck.py
+++ bin/healthcheck.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 """Simple healthcheck to see if localhost is responding."""
+import os
+import sys
+
 import requests
 
 
 def main():
     try:
-        return requests.get("http://localhost").status_code == 200
-    except:
-        return False
+        ex_code = os.EX_OK if requests.get("http://localhost/healthy").status_code == 200 else os.EX_NOHOST
+    except Exception:
+        ex_code = os.EX_NOHOST
+    sys.exit(ex_code)
 
 
 if __name__ == '__main__':
