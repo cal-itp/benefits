e508c25def32491ea91eca6ca274e9b58bf04cf1
Kegan Maher <kegan@compiler.la>
2021-05-12 17:09:19 -0700

    Fix API response parsing (#56)
    interpret HTTP response as text, not JSON
    
    the response body is the encrypted, encoded JWT text
    
    the test server was providing a red-herring response that could be parsed as JSON.

--- benefits/eligibility/api.py
+++ benefits/eligibility/api.py
@@ -75,7 +75,11 @@ class ResponseToken:
         logger.info("Read encrypted token from response")
 
         try:
-            encrypted_signed_token = response.json()
+            encrypted_signed_token = response.text
+            if not encrypted_signed_token:
+                raise ValueError()
+            # strip extra spaces and wrapping quote chars
+            encrypted_signed_token = encrypted_signed_token.strip("'\n\"")
         except ValueError:
             raise TokenError("Invalid response format")
 
@@ -139,6 +143,7 @@ class Client:
             raise TokenError("Failed to tokenize form values")
 
         try:
+            logger.debug(f"GET request to {self.verifier.api_url}")
             r = requests.get(self.verifier.api_url, headers=self._auth_headers(token))
         except requests.ConnectionError:
             raise ApiError("Connection to verification server failed")
