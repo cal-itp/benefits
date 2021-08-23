2abf5d63e397c5eab5ba28262a5f4cf79bc9c63c
Kegan Maher <kegan@compiler.la>
2021-08-23 21:27:55 +0000

    feat: move healthcheck to middleware
    prior to this change, ELB healthcheck requests were failing with 400
    as the HOST_HEADER was the ELB node's (internal) IP address
    
    this change moves the healthcheck to a middleware placed before the
    HOST_HEADER verification (in django.middleware.common.CommonMiddleware),
    so healthcheck requests always pass
    
    based on https://stackoverflow.com/a/64623669

--- benefits/settings.py
+++ benefits/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
+    "benefits.core.middleware.Healthcheck",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
