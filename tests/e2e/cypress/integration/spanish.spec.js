0242a14493e156f0df181bc6d83af573885c995e
Machiko Yasuda <machiko@compiler.la>
2021-11-02 18:35:44 +0000

    test: add Spanish test

---
+++ tests/e2e/cypress/integration/spanish.spec.js
@@ -0,0 +1,49 @@
+describe("Index page - Internationalization spec", () => {
+  it("Gives user Spanish language option", () => {
+    cy.visit("/")
+
+    cy.get("#header").should("contain", "Español")
+    cy.get("#header").should("not.contain", "English")
+  })
+
+  it("Changes the language to Spanish", () => {
+    cy.visit("/")
+
+    cy.contains("Español")
+      .click()
+
+    cy.contains("La nueva forma de pagar")
+    cy.get("#header").should("not.contain", "Español")
+    cy.get("#header").should("contain", "English")
+  })
+
+  it("Changes the language from Spanish back to English", () => {
+    cy.visit("/")
+
+    cy.contains("Español")
+      .click()
+
+    cy.contains("La nueva forma de pagar")
+    cy.get("#header").should("not.contain", "Español")
+    cy.get("#header").should("contain", "English")
+    cy.contains("English")
+      .click()
+
+    cy.get("#main-content").should("contain", "The new way to pay for transit")
+    cy.get("#main-content").should("not.contain", "La nueva forma de pagar")
+  })
+
+  it("Changes the language to Spanish from any page", () => {
+    cy.visit("/")
+
+    cy.get(".buttons .btn")
+      .first()
+      .click()
+
+    cy.contains("Español")
+      .click()
+
+    cy.get("#main-content").should("contain", "¡Hagámoslo!")
+    cy.get("#main-content").should("not.contain", "Let’s do it!")
+  })
+})
