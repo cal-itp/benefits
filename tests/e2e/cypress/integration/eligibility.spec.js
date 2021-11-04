8ed884576b0c6937978e00621aeebab7bf660c4d
Machiko Yasuda <machiko@compiler.la>
2021-11-04 21:00:37 +0000

    refactor: combine 2 specs into 1 file

--- tests/e2e/cypress/integration/eligibility.spec.js
+++ tests/e2e/cypress/integration/eligibility.spec.js
@@ -1,14 +1,14 @@
-describe("Eligibility page spec", () => {
-  it("Takes user to eligibility confirmation page", () => {
+describe("Confirms a user's eligibility status", () => {
+  beforeEach(() => {
     cy.visit("/")
-
     cy.get(".buttons .btn")
       .first()
       .click()
-
     cy.get(".buttons .btn")
       .click()
+  })
 
+  it("Takes user to eligibility confirmation page", () => {
     cy.contains("Ready to continue")
       .then(($e) => {
         expect($e).attr("href").to.have.string("eligibility/confirm")
@@ -17,7 +17,44 @@ describe("Eligibility page spec", () => {
     cy.get(".buttons .btn")
       .click()
 
-    cy.contains("Let’s see if we can confirm your age with the DMV")
+    cy.contains("Let’s see if we can confirm your age")
     cy.contains("CA driver’s license or ID number *")
+  })
+
+  it("From the confirmation page, confirms an eligible user", () => {
+    cy.get(".buttons .btn")
+      .click()
+    cy.get("#sub").type("A1234567")
+    cy.get("#name").type("Garcia")
+    cy.get("input[type='submit']")
+      .click()
+    cy.contains("Great! You’re eligible for a discount!")
+  })
+
+  it("From the confirmation page, confirms an ineligible user", () => {
+    cy.get(".buttons .btn")
+      .click()
+
+    cy.get("#sub").type("A1234567")
+    cy.get("#name").type("Bob")
+    cy.get("input[type='submit']")
+      .click()
+    cy.contains("We can’t confirm your age")
+    cy.contains("You may still be eligible for a discount, but we can’t verify your age")
+  })
+
+  it("From the confirmation page, validates an invalid number/id field", () => {
+    cy.get(".buttons .btn")
+      .click()
+
+    cy.get("#sub").type("A12347")
+    cy.get("#name").type("Garcia")
+    cy.get("input[type='submit']")
+      .click()
+    cy.contains("Check your input. The format looks wrong.")
+    cy.get("#id_sub")
+      .then(($e) => {
+        expect($e).to.have.css("border-color", "rgb(222, 12, 12)")
+      })
   })
 })
