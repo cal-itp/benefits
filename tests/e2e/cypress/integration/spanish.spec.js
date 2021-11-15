e6ef4c5915bd3111236b6129163119868964f1d2
Machiko Yasuda <machiko@compiler.la>
2021-11-15 23:46:15 +0000

    test(spanish): use variables

--- tests/e2e/cypress/integration/spanish.spec.js
+++ tests/e2e/cypress/integration/spanish.spec.js
@@ -1,3 +1,5 @@
+const agencies = require('../fixtures/transit-agencies.json')
+
 describe("Spanish language", () => {
   beforeEach(() => {
     cy.visit("/")
@@ -32,17 +34,17 @@ describe("Spanish language", () => {
     cy.get("#header").should("not.contain", "English")
   })
 
-  it("Changes the language to Spanish from any page", () => {
-    cy.get(".buttons .btn")
-      .first()
+  it.only("Changes the language to Spanish from any page", () => {
+    let agencySlug = agencies[0].slug
+    cy.contains(agencies[0].short_name)
       .click()
-      .location("pathname").should("eq", "/abc")
+      .location("pathname").should("eq", "/" + agencySlug)
 
     cy.contains("Español")
       .click()
 
     cy.get("#header").should("not.contain", "Español")
     cy.get("#header").should("contain", "English")
-    cy.location("pathname").should("eq", "/abc")
+    cy.location("pathname").should("eq", "/" + agencySlug)
   })
 })
