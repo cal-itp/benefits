9fc655ad14d3d568dac8c5eb147656c629c46d1d
Kegan Maher <kegan@compiler.la>
2021-11-29 20:09:00 +0000

    feat(admin): register new models in Django admin
    special registration for I18nText model

--- benefits/core/admin.py
+++ benefits/core/admin.py
@@ -7,6 +7,7 @@ from benefits.settings import ADMIN
 if ADMIN:
     import logging
     from django.contrib import admin
+    from modeltranslation.admin import TranslationAdmin
     from . import models
 
     logger = logging.getLogger(__name__)
@@ -14,9 +15,20 @@ if ADMIN:
     for model in [
         models.EligibilityType,
         models.EligibilityVerifier,
+        models.Icon,
+        models.Image,
+        models.MediaItem,
+        models.Page,
+        models.PageNavigation,
         models.PaymentProcessor,
         models.PemData,
         models.TransitAgency,
     ]:
         logger.debug(f"Register {model.__name__}")
         admin.site.register(model)
+
+    class I18nTextAdmin(TranslationAdmin):
+        pass
+
+    logger.debug(f"Register {models.I18nText.__name__} with admin class {I18nTextAdmin.__name__}")
+    admin.site.register(models.I18nText, I18nTextAdmin)
