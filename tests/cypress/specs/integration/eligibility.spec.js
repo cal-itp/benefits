ed9d97ecc9462b22390d175a3e1739f097ebdde9
Kegan Maher <kegan@compiler.la>
2022-01-22 19:21:23 +0000

    test(eligibility): verify rate limit using env
    get the allowed limit from environment, make many more requests than
    allowed in a short time to trigger the rate limit response (429)
    
    wait for the rate limit to reset using setting from environment, confirm
    that subsequent requests succeed (200)
    
    simplify beforeEach to not step through each page

--- tests/cypress/specs/integration/eligibility.spec.js
+++ tests/cypress/specs/integration/eligibility.spec.js
@@ -1,12 +1,12 @@
 const agencies = require("../../fixtures/transit-agencies");
 const users = require("../../fixtures/users.json");
 
+const eligibility_url = "/eligibility/confirm";
+
 describe("Eligibility Confirmation flow", () => {
   beforeEach(() => {
-    cy.visit("/");
-    cy.contains(agencies[0].fields.short_name).click();
-    cy.contains("Let’s do it!").click();
-    cy.contains("Ready to continue").click();
+    cy.visit("/" + agencies[0].fields.slug);
+    cy.visit(eligibility_url);
   });
 
   it("Confirms an eligible user", () => {
@@ -44,5 +44,78 @@ describe("Eligibility Confirmation flow", () => {
     cy.get("input[type='submit']").click();
 
     cy.get("input:invalid").should("have.length", 1);
+  });
+
+  it("Rate limits excess requests", () => {
+    const sub = users.invalidSub.sub;
+    const name = users.invalidSub.name;
+
+    // start by making 5 times as many requests as allowed
+    const RATE_LIMIT = Cypress.env("DJANGO_RATE_LIMIT");
+    const N_REQUESTS = RATE_LIMIT * 5;
+
+    // csrfmiddlewaretoken value needed to enable automated form submissions
+    // there could be multiple forms on the page, so get() the one with the
+    // action we are interested in
+    cy.get(`form[action='${eligibility_url}'`)
+      .find("input[name='csrfmiddlewaretoken']")
+      .invoke("attr", "value")
+      .then((csrfmiddlewaretoken) => {
+        // make excess requests
+        for (let i = 0; i < N_REQUESTS; i++) {
+          cy.request({
+            method: "POST",
+            url: eligibility_url,
+            followRedirect: false,
+            // don't raise an exception for non-success status codes so we can inspect the response
+            failOnStatusCode: false,
+            // submit body as application/x-www-form-urlencoded
+            form: true,
+            // the same body a user-initiated form-submission sends
+            body: {
+              csrfmiddlewaretoken,
+              sub,
+              name,
+            },
+          }).then((res) => {
+            if (i < RATE_LIMIT) {
+              // allow up to API_RATE_LIMIT requests
+              expect(res.status).to.eq(200);
+            } else {
+              // we've gone beyond API_RATE_LIMIT
+              expect(res.status).to.eq(429);
+            }
+          });
+        }
+
+        // now wait for the rate limit to refresh
+        const RATE_LIMIT_PERIOD = parseInt(
+          Cypress.env("DJANGO_RATE_LIMIT_PERIOD")
+        );
+        // Cypress expects milliseconds, the rate limit period is expressed in seconds
+        cy.wait(RATE_LIMIT_PERIOD * 1000);
+
+        // and try again with a reasonable number of requests
+        for (let i = 0; i < RATE_LIMIT - 1; i++) {
+          cy.request({
+            method: "POST",
+            url: eligibility_url,
+            followRedirect: false,
+            // we expect all of these requests to succeed
+            failOnStatusCode: true,
+            // submit body as application/x-www-form-urlencoded
+            form: true,
+            // the same body a user-initiated form-submission sends
+            body: {
+              csrfmiddlewaretoken,
+              sub,
+              name,
+            },
+          }).then((res) => {
+            // these requests should all succeed
+            expect(res.status).to.eq(200);
+          });
+        }
+      });
   });
 });
