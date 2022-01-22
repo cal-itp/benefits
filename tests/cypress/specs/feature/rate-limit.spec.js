4f52866512279801efba5033cd719fc83ccc0f30
Kegan Maher <kegan@compiler.la>
2022-01-22 20:23:40 +0000

    refactor(tests): organize as feature specs
    * move simple form tests to eligibility UI spec
    * rename integration --> feature
    * rename test to reflect that it tests the rate limiting feature

--- tests/cypress/specs/feature/rate-limit.spec.js
+++ tests/cypress/specs/feature/rate-limit.spec.js
@@ -3,50 +3,13 @@ const users = require("../../fixtures/users.json");
 
 const eligibility_url = "/eligibility/confirm";
 
-describe("Eligibility Confirmation flow", () => {
+describe("Rate limiting feature spec", () => {
   beforeEach(() => {
     cy.visit("/" + agencies[0].fields.slug);
     cy.visit(eligibility_url);
   });
 
-  it("Confirms an eligible user", () => {
-    cy.get("#sub").type(users.eligible.sub);
-    cy.get("#name").type(users.eligible.name);
-    cy.get("input[type='submit']").click();
-
-    cy.contains("Great! You’re eligible for a discount!");
-  });
-
-  it("Rejects an ineligible user", () => {
-    cy.get("#sub").type(users.ineligible.sub);
-    cy.get("#name").type(users.ineligible.name);
-    cy.get("input[type='submit']").click();
-
-    cy.contains("We can’t confirm your age");
-    cy.contains(
-      "You may still be eligible for a discount, but we can’t verify your age"
-    );
-  });
-
-  it("Validates an invalid number/id field", () => {
-    cy.get("#sub").type(users.invalidSub.sub);
-    cy.get("#name").type(users.invalidSub.name);
-    cy.get("input[type='submit']").click();
-
-    cy.contains("Check your input. The format looks wrong.");
-    cy.get("#id_sub").then(($e) => {
-      expect($e).to.have.css("border-color", "rgb(222, 12, 12)");
-    });
-  });
-
-  it("Validates an empty name field", () => {
-    cy.get("#sub").type(users.invalidSub.sub);
-    cy.get("input[type='submit']").click();
-
-    cy.get("input:invalid").should("have.length", 1);
-  });
-
-  it("Rate limits excess requests", () => {
+  it("Limits excess requests", () => {
     const sub = users.invalidSub.sub;
     const name = users.invalidSub.name;
 
--- tests/cypress/specs/ui/eligibility.spec.js
+++ tests/cypress/specs/ui/eligibility.spec.js
@@ -1,29 +1,72 @@
 const agencies = require("../../fixtures/transit-agencies");
+const users = require("../../fixtures/users.json");
 
-describe("Eligibility Confirmation page spec", () => {
-  beforeEach(() => {
+const eligibility_url = "/eligibility/confirm";
+
+describe("Eligibility confirmation page spec", () => {
+  it("User can navigate to the confirmation page", () => {
     cy.visit("/");
     cy.contains(agencies[0].fields.short_name).click();
     cy.contains("Let’s do it!").click();
     cy.contains("Ready to continue").click();
+    cy.url().should("include", eligibility_url);
+  });
+});
+
+describe("Eligibility confirmation form spec", () => {
+  beforeEach(() => {
+    cy.visit("/" + agencies[0].fields.slug);
+    cy.visit(eligibility_url);
   });
 
-  it("Takes user to eligibility confirmation page", () => {
-    cy.contains("Let’s see if we can confirm your age");
-    cy.contains("CA driver’s license or ID number *");
-  });
-
-  it("Confirmation page has a form with a driver’s license or ID number form label and corresponding form field", () => {
+  it("Has a driver’s license or ID number form label and corresponding field", () => {
     cy.get("input:focus").should("have.length", 0);
     cy.contains("CA driver’s license or ID number *").click();
 
     cy.get("input:focus").should("have.length", 1);
   });
 
-  it("Confirmation page has a form with a lastname form label and corresponding form field", () => {
+  it("Has a last name form label and corresponding form field", () => {
     cy.get("input:focus").should("have.length", 0);
     cy.contains("Last name (as it appears on ID) *").click();
 
     cy.get("input:focus").should("have.length", 1);
+  });
+
+  it("Confirms an eligible user", () => {
+    cy.get("#sub").type(users.eligible.sub);
+    cy.get("#name").type(users.eligible.name);
+    cy.get("input[type='submit']").click();
+
+    cy.contains("Great! You’re eligible for a discount!");
+  });
+
+  it("Rejects an ineligible user", () => {
+    cy.get("#sub").type(users.ineligible.sub);
+    cy.get("#name").type(users.ineligible.name);
+    cy.get("input[type='submit']").click();
+
+    cy.contains("We can’t confirm your age");
+    cy.contains(
+      "You may still be eligible for a discount, but we can’t verify your age"
+    );
+  });
+
+  it("Validates an invalid number/id field", () => {
+    cy.get("#sub").type(users.invalidSub.sub);
+    cy.get("#name").type(users.invalidSub.name);
+    cy.get("input[type='submit']").click();
+
+    cy.contains("Check your input. The format looks wrong.");
+    cy.get("#id_sub").then(($e) => {
+      expect($e).to.have.css("border-color", "rgb(222, 12, 12)");
+    });
+  });
+
+  it("Validates an empty name field", () => {
+    cy.get("#sub").type(users.invalidSub.sub);
+    cy.get("input[type='submit']").click();
+
+    cy.get("input:invalid").should("have.length", 1);
   });
 });
