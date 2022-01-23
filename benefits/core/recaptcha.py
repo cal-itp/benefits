a1527d7d71cf34a7f8eb1f4ed76cf1e2fe3140e5
Kegan Maher <kegan@compiler.la>
2022-01-23 07:16:55 +0000

    feat(recaptcha): helper to verify recaptcha token
    token passed to server-side with a form submission, submit to reCAPTCHA
    service to verify; see https://developers.google.com/recaptcha/docs/verify

---
+++ benefits/core/recaptcha.py
@@ -0,0 +1,23 @@
+"""
+The core application: helpers to work with reCAPTCHA.
+"""
+import requests
+
+from benefits.settings import RECAPTCHA_ENABLED, RECAPTCHA_SECRET_KEY, RECAPTCHA_VERIFY_URL
+
+
+_POST_DATA = "g-recaptcha-response"
+
+
+def verify(form_data: dict) -> bool:
+    """
+    Check with Google reCAPTCHA if the given response is a valid user.
+    See https://developers.google.com/recaptcha/docs/verify
+    """
+    if any((not RECAPTCHA_ENABLED, not form_data, _POST_DATA not in form_data)):
+        return True
+
+    payload = dict(secret=RECAPTCHA_SECRET_KEY, response=form_data[_POST_DATA])
+    response = requests.post(RECAPTCHA_VERIFY_URL, payload).json()
+
+    return bool(response["success"])
