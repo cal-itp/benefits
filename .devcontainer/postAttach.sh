7f1ea62fb740138bfcf74aa8f17455bce7455066
Aidan Feldman <aidan.feldman@gmail.com>
2022-04-05 00:00:26 +0000

    fix: handle global gitconfig with hooksPath

--- .devcontainer/postAttach.sh
+++ .devcontainer/postAttach.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -eu
 
+# The global gitconfig gets copied into the container:
+#
+# https://code.visualstudio.com/docs/remote/containers#_sharing-git-credentials-with-your-container
+#
+# and may have a hooksPath setting that is incompatible with pre-commit.
+#
+# https://github.com/pre-commit/pre-commit/issues/1198#issuecomment-547478570
+#
+# Remove it if present.
+git config --global --unset-all core.hooksPath
+
 # initialize hook environments
 pre-commit install --install-hooks --overwrite
 
