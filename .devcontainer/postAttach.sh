755ec2de8f65e56547abc1ecd5bf164eface1400
Kegan Maher <kegan@compiler.la>
2022-04-06 15:42:35 +0000

    feat(devcontainer): copy oidc-client into devcontainer
    when building the app container, the library is copied into a temp location
    before static files are processed by Django
    
    the devcontainer handles static files differently, so we copy the scripts
    into a location it can see

--- .devcontainer/postAttach.sh
+++ .devcontainer/postAttach.sh
@@ -7,5 +7,8 @@ pre-commit install --install-hooks --overwrite
 # manage commit-msg hooks
 pre-commit install --hook-type commit-msg
 
+# copy Login.gov client library
+cp node_modules/oidc-client-ts/dist/browser/oidc-client-ts* benefits/static/js
+
 # install cypress
 cd tests/cypress && npm install && npx cypress install
