73bd5a1a34ae35d461d22f01cc8c198571469c4d
Kegan Maher <kegan@compiler.la>
2022-04-06 21:54:05 +0000

    perf(devcontainer): prioritize npm install
    helps make the app runnable faster in the startup cycle

--- .devcontainer/postAttach.sh
+++ .devcontainer/postAttach.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -eu
 
+echo [install npm packages for app]
+npm ci
+mkdir -p benefits/static/js
+cp node_modules/oidc-client-ts/dist/browser/oidc-client-ts* benefits/static/js
+
 echo [initialize hook environments]
 pre-commit install --install-hooks --overwrite
 
 echo [manage commit-msg hooks]
 pre-commit install --hook-type commit-msg
-
-echo [install npm packages for app]
-npm ci
-mkdir -p benefits/static/js
-cp node_modules/oidc-client-ts/dist/browser/oidc-client-ts* benefits/static/js
 
 echo [install npm packages for tests]
 cd tests/cypress
