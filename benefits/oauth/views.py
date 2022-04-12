8b089afd66619489952efe1b52d818870996b577
Kegan Maher <kegan@compiler.la>
2022-04-12 00:23:45 +0000

    refactor(oauth): simplify client and session management
    * use the new setting for client name
    * create the client once for the module if client name configured
    * raise exception if client cannot be created but is needed
    * remove need to check session for verifier or use verifier

--- benefits/oauth/views.py
+++ benefits/oauth/views.py
@@ -1,41 +1,30 @@
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.utils.decorators import decorator_from_middleware
 
 from authlib.integrations.django_client import OAuth
 
-from benefits.core import middleware, session
+from benefits.core import session
+from benefits.settings import OAUTH_CLIENT_NAME
 
 
 _oauth = OAuth()
-_oauth.register("oidc")
+_oauth.register(OAUTH_CLIENT_NAME)
+oauth_client = _oauth.create_client(OAUTH_CLIENT_NAME)
 
 
-@decorator_from_middleware(middleware.AgencySessionRequired)
-@decorator_from_middleware(middleware.VerifierSessionRequired)
 def login(request):
-    verifier = session.verifier(request)
-    if not verifier.requires_authentication:
-        return redirect(session.origin(request))
-
-    oauth_client = _oauth.create_client(verifier.auth_provider.name)
-
     if not oauth_client:
-        return redirect(session.origin(request))
+        raise Exception("No OAuth client")
 
     route = reverse("oauth:authorize")
     redirect_uri = request.build_absolute_uri(route)
+
     return oauth_client.authorize_redirect(request, redirect_uri)
 
 
-@decorator_from_middleware(middleware.AgencySessionRequired)
-@decorator_from_middleware(middleware.VerifierSessionRequired)
 def authorize(request):
-    verifier = session.verifier(request)
-    if not verifier.requires_authentication:
-        return redirect(session.origin(request))
-
-    oauth_client = _oauth.create_client(verifier.auth_provider.name)
+    if not oauth_client:
+        raise Exception("No OAuth client")
 
     token = oauth_client.authorize_access_token(request)
 
