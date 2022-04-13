d068cd93ccc0903d81dab6ca71f0d18372e30b27
Machiko Yasuda <machiko@compiler.la>
2022-04-13 22:25:41 +0000

    test(enrollment/success): add copy specs and sutub out Login.gov spec

--- tests/cypress/specs/ui/enrollment.spec.js
+++ tests/cypress/specs/ui/enrollment.spec.js
@@ -3,14 +3,18 @@ describe("Enrollment success page", () => {
     cy.visit("/enrollment/success");
   });
 
-  it("Has the appropriate English copy", () => {
+  it("Has the appropriate English copy for all flows", () => {
     cy.contains("Success! Your discount is now linked to your bank card.");
+    cy.contains("You were not charged anything today.");
   });
 
-  it("Has the appropriate Spanish copy", () => {
+  it("Has the appropriate Spanish copy for all flows", () => {
     cy.contains("Español").click();
     cy.contains(
       "Felicidades! Su descuento ahora está vinculado a su tarjeta bancaria."
     );
+    cy.contains("No te cobraron nada hoy.");
   });
+
+  it("If there is an AuthProvider, show the Login.gov sign out instructions", () => {});
 });
