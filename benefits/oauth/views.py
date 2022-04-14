5c7f5b5ee26de5c3e71223b6214d483df7cb204e
Kegan Maher <kegan@compiler.la>
2022-04-14 18:23:07 +0000

    feat(oauth): helper implements OIDC end_session_endpoint

--- benefits/oauth/views.py
+++ benefits/oauth/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils.http import urlencode
 
 from authlib.integrations.django_client import OAuth
 
@@ -59,3 +60,24 @@ def logout(request):
 
     session.update(request, auth=False)
     return redirect(ROUTE_START)
+
+
+def _deauthorize_redirect(token, redirect_url):
+    """Helper implements OIDC signout via the `end_session_endpoint`."""
+
+    # Authlib has not yet implemented `end_session_endpoint` as the OIDC Session Management 1.0 spec is still in draft
+    # See https://github.com/lepture/authlib/issues/331#issuecomment-827295954 for more
+    #
+    # The implementation here was adapted from the same ticket: https://github.com/lepture/authlib/issues/331#issue-838728145
+
+    if not oauth_client:
+        raise Exception("No OAuth client")
+
+    metadata = oauth_client.load_server_metadata()
+    end_session_endpoint = metadata.get("end_session_endpoint")
+
+    params = dict(id_token_hint=token["id_token"], post_logout_redirect_uri=redirect_url)
+    encoded_params = urlencode(params)
+    end_session_url = f"{end_session_endpoint}?{encoded_params}"
+
+    return redirect(end_session_url)
