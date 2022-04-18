96dc1851acbb49a2bd154b9f3bc76b640265953a
Kegan Maher <kegan@compiler.la>
2022-04-18 20:57:29 +0000

    feat(oauth): define custom events for sign in/out flow

--- benefits/oauth/analytics.py
+++ benefits/oauth/analytics.py
@@ -10,3 +10,21 @@ class OAuthEvent(core.Event):
         """Base OAuth analytics event."""
         super().__init__(request, event_type)
         self.update_event_properties(auth_provider=OAUTH_CLIENT_NAME)
+
+
+class StartedSignInEvent(OAuthEvent):
+    def __init__(self, request):
+        """Analytics event representing the beginning of the OAuth sign in flow."""
+        super().__init__(request, "started sign in")
+
+
+class FinishedSignInEvent(OAuthEvent):
+    def __init__(self, request):
+        """Analytics event representing the end of the OAuth sign in flow."""
+        super().__init__(request, "finished sign in")
+
+
+class SignedOutEvent(OAuthEvent):
+    def __init__(self, request):
+        """Analytics event representing application sign out."""
+        super().__init__(request, "signed out")
