058b07b2699b076f1a4fcd850c86e05adbef2703
Kegan Maher <kegan@compiler.la>
2022-04-18 16:04:52 +0000

    fix(settings): add SECURE_PROXY_SSL_HEADER for nginx
    the NGINX reverse proxy sits in front of the application in deployed environments
    SSL terminates before getting to Django, and NGINX adds this header to indicate
    if the original request was secure or not
    
    See https://docs.djangoproject.com/en/3.2/ref/settings/#secure-proxy-ssl-header

--- benefits/settings.py
+++ benefits/settings.py
@@ -90,6 +90,14 @@ if not DEBUG:
 
 SECURE_BROWSER_XSS_FILTER = True
 
+# the NGINX reverse proxy sits in front of the application in deployed environments
+# SSL terminates before getting to Django, and NGINX adds this header to indicate
+# if the original request was secure or not
+#
+# See https://docs.djangoproject.com/en/3.2/ref/settings/#secure-proxy-ssl-header
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 ROOT_URLCONF = "benefits.urls"
 
 template_ctx_processors = [
