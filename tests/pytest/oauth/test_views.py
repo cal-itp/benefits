2e291f2c50007e743fb19aba389bdee41149d4a8
Kegan Maher <kegan@compiler.la>
2022-04-21 17:21:06 +0000

    test(oauth): http and https generate_redirect_uri

--- tests/pytest/oauth/test_views.py
+++ tests/pytest/oauth/test_views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.urls import reverse
 
 from benefits.core import session
-from benefits.oauth.views import logout, post_logout
+from benefits.oauth.views import logout, post_logout, _generate_redirect_uri
 
 import pytest
 
@@ -21,7 +21,7 @@ def test_logout(mocker, session_request):
 
     result = logout(session_request)
 
-    spy.assert_called_with(token, "http://testserver/oauth/post_logout")
+    spy.assert_called_with(token, "https://testserver/oauth/post_logout")
     assert result.status_code == 200
     assert message in str(result.content)
     assert session.oauth_token(session_request) is False
@@ -36,3 +36,21 @@ def test_post_logout(session_request):
 
     assert result.status_code == 302
     assert result.url == origin
+
+
+def test_generate_redirect_uri_default(rf):
+    request = rf.get("/oauth/login")
+    path = "/test"
+
+    redirect_uri = _generate_redirect_uri(request, path)
+
+    assert redirect_uri == "https://testserver/test"
+
+
+def test_generate_redirect_uri_localhost(rf):
+    request = rf.get("/oauth/login", SERVER_NAME="localhost")
+    path = "/test"
+
+    redirect_uri = _generate_redirect_uri(request, path)
+
+    assert redirect_uri == "http://localhost/test"
