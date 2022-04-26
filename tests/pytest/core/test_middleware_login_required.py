a7afc14fff094cbc44576986d225ff9dd6be334b
Aidan Feldman <aidan@compiler.la>
2022-04-26 21:59:55 +0000

    refactor(tests): use app_request for login tests

--- tests/pytest/core/test_middleware_login_required.py
+++ tests/pytest/core/test_middleware_login_required.py
@@ -7,8 +7,6 @@ from benefits.core import session
 from benefits.core.middleware import LoginRequired
 from benefits.core.models import EligibilityVerifier
 
-from tests.pytest.conftest import initialize_request
-
 login_path = reverse("oauth:login")
 
 
@@ -25,13 +23,9 @@ def require_login(request):
 
 
 @pytest.mark.django_db
-def test_login_auth_required(rf, mocked_view, decorated_view):
-    blocked_path = "/some/blocked/path"
-    request = rf.get(blocked_path)
-    initialize_request(request)
-    require_login(request)
-
-    response = decorated_view(request)
+def test_login_auth_required(app_request, mocked_view, decorated_view):
+    require_login(app_request)
+    response = decorated_view(app_request)
 
     mocked_view.assert_not_called()
 
@@ -40,30 +34,23 @@ def test_login_auth_required(rf, mocked_view, decorated_view):
 
 
 @pytest.mark.django_db
-def test_login_auth_not_required(rf, mocked_view, decorated_view):
-    blocked_path = "/some/blocked/path"
-    request = rf.get(blocked_path)
-    initialize_request(request)
-
+def test_login_auth_not_required(app_request, mocked_view, decorated_view):
     verifier = EligibilityVerifier.objects.filter(auth_provider__isnull=True).first()
     assert verifier
     assert not verifier.requires_authentication
-    session.update(request, verifier=verifier)
+    session.update(app_request, verifier=verifier)
 
-    decorated_view(request)
+    decorated_view(app_request)
 
     mocked_view.assert_called_once()
 
 
 @pytest.mark.django_db
-def test_logged_in(rf, mocked_view, decorated_view):
-    blocked_path = "/some/blocked/path"
-    request = rf.get(blocked_path)
-    initialize_request(request)
-    require_login(request)
+def test_logged_in(app_request, mocked_view, decorated_view):
+    require_login(app_request)
 
     # log in
-    session.update(request, oauth_token="something")
+    session.update(app_request, oauth_token="something")
 
-    decorated_view(request)
+    decorated_view(app_request)
     mocked_view.assert_called_once()
