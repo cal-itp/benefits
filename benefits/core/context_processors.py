c5bdb7bb3a3bed3aa66e1464f46d95f36b6a600c
Angela Tran <angela@compiler.la>
2022-04-28 22:59:02 +0000

    feat: add new includes template for sign out link, and use where needed

--- benefits/core/context_processors.py
+++ benefits/core/context_processors.py
@@ -2,6 +2,7 @@
 The core application: context processors for enriching request context data.
 """
 from django.conf import settings
+from django.urls import reverse
 
 from . import session
 
@@ -9,6 +10,38 @@ from . import session
 def analytics(request):
     """Context processor adds some analytics information to request context."""
     return {"analytics": {"api_key": settings.ANALYTICS_KEY, "uid": session.uid(request), "did": session.did(request)}}
+
+
+def authentication(request):
+    """Context processor adds authentication information to request context."""
+    verifier = session.verifier(request)
+
+    if verifier:
+        return {
+            "authentication": {
+                "required": verifier.requires_authentication,
+                "logged_in": session.logged_in(request),
+                "sign_out_route": reverse("oauth:logout"),
+            }
+        }
+    else:
+        return {}
+
+
+def auth_provider(request):
+    """Context processor adds auth_provider information to request context."""
+    verifier = session.verifier(request)
+
+    if verifier and verifier.requires_authentication:
+        auth_provider = verifier.auth_provider
+        return {
+            "auth_provider": {
+                "sign_in_button_label": auth_provider.sign_in_button_label,
+                "sign_out_button_label": auth_provider.sign_out_button_label,
+            }
+        }
+    else:
+        return {}
 
 
 def debug(request):
