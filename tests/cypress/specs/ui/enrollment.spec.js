0c153643237fa0807052292f64521a3765bb6a97
Machiko Yasuda <machiko@compiler.la>
2022-04-28 00:17:42 +0000

    test: remove skipped specs

--- tests/cypress/specs/ui/enrollment.spec.js
+++ tests/cypress/specs/ui/enrollment.spec.js
@@ -23,30 +23,3 @@ describe("Single Verifier, No AuthProvider: Enrollment success page", () => {
     cy.contains("No te cobraron nada hoy.");
   });
 });
-
-describe.skip("Multiple Verifier, with AuthProvider: Enrollment success page", () => {
-  beforeEach(() => {
-    cy.visit("/");
-    // Selecting ABC will go down the multiple verifier flow
-    cy.contains(agencies[0].fields.short_name).click();
-    cy.contains("Get started").click();
-    // Selecting Senior Discount will go down the auth provider flow
-    cy.get("input:radio").first().click();
-    cy.contains("Continue").click();
-    cy.contains("Create an account or sign in with");
-    cy.visit("/enrollment/success");
-  });
-
-  it("Has the appropriate English copy for all flows and Login.gov instructions", () => {
-    cy.contains("Success! Your discount is now linked to your bank card.");
-    cy.contains("You were not charged anything today.");
-  });
-
-  it("Has the appropriate Spanish copy for all flows and Login.gov instructions", () => {
-    cy.contains("Español").click();
-    cy.contains(
-      "Felicidades! Su descuento ahora está vinculado a su tarjeta bancaria."
-    );
-    cy.contains("No te cobraron nada hoy.");
-  });
-});
