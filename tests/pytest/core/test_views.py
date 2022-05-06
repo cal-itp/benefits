ceff8bc87b9769e63af86acef8fd0a98fc227f38
Aidan Feldman <aidan@compiler.la>
2022-05-06 16:22:07 +0000

    chore: add tests for help page

--- tests/pytest/core/test_views.py
+++ tests/pytest/core/test_views.py
@@ -1,10 +1,33 @@
 import pytest
 from django.urls import reverse
+from benefits.core.models import TransitAgency
+
+from tests.pytest.conftest import with_agency
 
 
 @pytest.mark.django_db
 def test_homepage(client):
-    homepage = reverse("core:index")
-    response = client.get(homepage)
+    path = reverse("core:index")
+    response = client.get(path)
     assert response.status_code == 200
     assert "transit" in str(response.content)
+
+
+@pytest.mark.django_db
+def test_help(client):
+    path = reverse("core:help")
+    response = client.get(path)
+    assert response.status_code == 200
+    assert "card" in str(response.content)
+
+
+@pytest.mark.django_db
+def test_help_with_agency(mocker, client):
+    agency = TransitAgency.objects.first()
+    assert agency
+    with_agency(mocker, agency)
+
+    path = reverse("core:help")
+    response = client.get(path)
+    assert response.status_code == 200
+    assert agency.long_name in str(response.content)
