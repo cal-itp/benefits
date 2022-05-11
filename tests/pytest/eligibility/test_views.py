6a29e6a42401482996a7c7eba750b39fd6a81e93
Angela Tran <angela@compiler.la>
2022-05-11 00:53:33 +0000

    test(eligibility): add test for success scenario

--- tests/pytest/eligibility/test_views.py
+++ tests/pytest/eligibility/test_views.py
@@ -1,8 +1,11 @@
 from django.urls import reverse
 import pytest
+from typing import Tuple
 
-from benefits.core.models import TransitAgency
-from tests.pytest.conftest import with_agency
+from benefits.core import session
+from benefits.core.models import TransitAgency, EligibilityVerifier
+from benefits.eligibility.views import confirm
+from tests.pytest.conftest import with_agency, initialize_request
 
 
 def set_agency(mocker):
@@ -12,13 +15,14 @@ def set_agency(mocker):
     return agency
 
 
-def set_verifier(mocker):
+def set_verifier(mocker) -> Tuple[TransitAgency, EligibilityVerifier]:
     agency = set_agency(mocker)
 
     mock = mocker.patch("benefits.core.session.verifier", autospec=True)
     verifier = agency.eligibility_verifiers.first()
     assert verifier
     mock.return_value = verifier
+    return (agency, verifier)
 
 
 @pytest.mark.django_db
@@ -52,3 +56,21 @@ def test_start_without_verifier(mocker, client):
     path = reverse("eligibility:start")
     with pytest.raises(AttributeError, match=r"verifier"):
         client.get(path)
+
+
+@pytest.mark.django_db
+def test_confirm_success(mocker, rf):
+    agency, verifier = set_verifier(mocker)
+
+    path = reverse("eligibility:confirm")
+    # currently, this corresponds to values from eligibility-server
+    body = {"sub": "A1234567", "name": "Garcia"}
+    request = rf.post(path, body)
+
+    initialize_request(request)
+    session.update(request, agency=agency, verifier=verifier, oauth_token="token")
+
+    response = confirm(request)
+
+    assert response.status_code == 302
+    assert response.url == reverse("enrollment:index")
