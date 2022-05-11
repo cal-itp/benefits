64b84e54ce464530164f18c833960433c3cef9b2
Angela Tran <angela@compiler.la>
2022-05-11 20:11:48 +0000

    test(eligibility): mock eligibility-server response
    Also, remove comments about POST body needing to match server
    implementation.
    
    Our tests should not require a real instance to be up and running, and
    in particular, our CI configuration is such that we will not have one.

--- tests/pytest/eligibility/test_views.py
+++ tests/pytest/eligibility/test_views.py
@@ -1,5 +1,13 @@
 from django.urls import reverse
 import pytest
+import httpretty
+
+import datetime
+import os
+import uuid
+
+from pathlib import Path
+from jwcrypto import jwk, jwt
 from typing import Tuple
 
 from benefits.core import session
@@ -20,6 +28,7 @@ def set_verifier(mocker) -> Tuple[TransitAgency, EligibilityVerifier]:
 
     mock = mocker.patch("benefits.core.session.verifier", autospec=True)
     verifier = agency.eligibility_verifiers.first()
+    mocker.patch.object(verifier, "api_url", "http://localhost/verify")
     assert verifier
     mock.return_value = verifier
     return (agency, verifier)
@@ -58,13 +67,34 @@ def test_start_without_verifier(mocker, client):
         client.get(path)
 
 
+@httpretty.activate(verbose=True, allow_net_connect=False)
 @pytest.mark.django_db
 def test_confirm_success(mocker, rf):
     agency, verifier = set_verifier(mocker)
 
+    # Mock the eligibility-server response using HTTPretty
+    # https://stackoverflow.com/questions/21877387/mocking-a-http-server-in-python
+    httpretty.register_uri(
+        httpretty.GET,
+        "http://localhost/verify",
+        status=200,
+        body=_make_token(
+            {
+                "jti": str(uuid.uuid4()),
+                "iss": "test-server",
+                "iat": int(datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).timestamp()),
+                "eligibility": ["type1"],
+            },
+            verifier.jws_signing_alg,
+            _get_jwk("server.key"),
+            verifier.jwe_encryption_alg,
+            verifier.jwe_cek_enc,
+            _get_jwk("client.pub"),
+        ),
+    )
+
     path = reverse("eligibility:confirm")
-    # currently, this corresponds to values from eligibility-server
-    body = {"sub": "A1234567", "name": "Garcia"}
+    body = {"sub": "A0101011", "name": "Lastname"}
     request = rf.post(path, body)
 
     initialize_request(request)
@@ -74,3 +104,28 @@ def test_confirm_success(mocker, rf):
 
     assert response.status_code == 302
     assert response.url == reverse("enrollment:index")
+
+
+def _get_jwk(filename):
+    current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+    file_path = current_path / "keys" / filename
+
+    with file_path.open(mode="rb") as pemfile:
+        key = jwk.JWK.from_pem(pemfile.read())
+
+    return key
+
+
+# copied and pasted from eligibility-server code for now - replace with eligibility-api function when available
+def _make_token(payload, jws_signing_alg, server_private_key, jwe_encryption_alg, jwe_cek_enc, client_public_key):
+    """Wrap payload in a signed and encrypted JWT for response."""
+    # sign the payload with server's private key
+    header = {"typ": "JWS", "alg": jws_signing_alg}
+    signed_token = jwt.JWT(header=header, claims=payload)
+    signed_token.make_signed_token(server_private_key)
+    signed_payload = signed_token.serialize()
+    # encrypt the signed payload with client's public key
+    header = {"typ": "JWE", "alg": jwe_encryption_alg, "enc": jwe_cek_enc}
+    encrypted_token = jwt.JWT(header=header, claims=signed_payload)
+    encrypted_token.make_encrypted_token(client_public_key)
+    return encrypted_token.serialize()
