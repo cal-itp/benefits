a387ed002bf1d04b2058a32fa5cdd51b739cfcba
Aidan Feldman <aidan@compiler.la>
2022-06-02 17:26:31 -0400

    refactor: compute the failed location count programmatically

--- terraform/uptime.tf
+++ terraform/uptime.tf
@@ -11,6 +11,9 @@ resource "azurerm_application_insights_web_test" "dev_healthcheck" {
   application_insights_id = data.azurerm_application_insights.benefits.id
   kind                    = "ping"
   enabled                 = true
+
+  # "We strongly recommend testing from … a minimum of five locations."
+  # https://docs.microsoft.com/en-us/azure/azure-monitor/app/monitor-web-app-availability#create-a-test
   geo_locations = [
     "us-fl-mia-edge", # Central US
     "us-va-ash-azr",  # East US
@@ -45,7 +48,9 @@ resource "azurerm_monitor_metric_alert" "uptime" {
   application_insights_web_test_location_availability_criteria {
     web_test_id           = azurerm_application_insights_web_test.dev_healthcheck.id
     component_id          = data.azurerm_application_insights.benefits.id
-    failed_location_count = 3
+    # "the optimal configuration is to have the number of test locations be equal to the alert location threshold + 2"
+    # https://docs.microsoft.com/en-us/azure/azure-monitor/app/monitor-web-app-availability#create-a-test
+    failed_location_count = length(azurerm_application_insights_web_test.dev_healthcheck.geo_locations) - 2
   }
 
   action {
