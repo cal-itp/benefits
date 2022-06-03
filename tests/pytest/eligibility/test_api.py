4391a815c59e76b50da5781a87bc0091307943b4
Kegan Maher <kegan@compiler.la>
2022-06-03 06:24:04 +0000

    feat(tests): helper mocks client.verify

--- tests/pytest/eligibility/test_api.py
+++ tests/pytest/eligibility/test_api.py
@@ -6,3 +6,7 @@ from benefits.eligibility.forms import EligibilityVerificationForm
 @pytest.fixture
 def form(mocker):
     return mocker.Mock(spec=EligibilityVerificationForm, cleaned_data={"name": "Garcia", "sub": "A1234567"})
+
+
+def mock_api_client_verify(mocker, response):
+    return mocker.patch("benefits.eligibility.api.Client.verify", return_value=response)
