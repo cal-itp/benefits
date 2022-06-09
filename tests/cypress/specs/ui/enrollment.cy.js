8a953900dad88775a3ee7b39febc8271409eb710
Machiko Yasuda <machiko@compiler.la>
2022-06-09 22:02:56 +0000

    test(enrollment): single verifier, no autthprovider flow test - remove Continue page call

--- tests/cypress/specs/ui/enrollment.cy.js
+++ tests/cypress/specs/ui/enrollment.cy.js
@@ -5,7 +5,6 @@ describe("Single Verifier, No AuthProvider: Enrollment success page", () => {
     cy.visit("/");
     // Selecting DEFTl will go down the single verifier flow
     cy.contains(agencies[1].fields.short_name).click();
-    cy.contains("Get started").click();
     cy.contains("Continue").click();
     // https://github.com/cypress-io/cypress/issues/18690
     cy.window().then((win) => (win.location.href = "/enrollment/success"));
