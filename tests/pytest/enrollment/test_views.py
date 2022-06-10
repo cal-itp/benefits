351164cdb13656d02c14b91e0f568659bdef9564
Kegan Maher <kegan@compiler.la>
2022-06-10 18:46:51 +0000

    refactor(tests/enrollment/views): use route constants

--- tests/pytest/enrollment/test_views.py
+++ tests/pytest/enrollment/test_views.py
@@ -3,16 +3,19 @@ from django.urls import reverse
 import pytest
 
 
+ROUTE_INDEX = "enrollment:index"
+
+
 @pytest.mark.django_db
 @pytest.mark.usefixtures("mocked_session_agency", "mocked_session_eligibility")
 def test_index_eligible(client):
-    path = reverse("enrollment:index")
+    path = reverse(ROUTE_INDEX)
     response = client.get(path)
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_index_ineligible(client):
-    path = reverse("enrollment:index")
+    path = reverse(ROUTE_INDEX)
     with pytest.raises(AttributeError, match=r"eligibility"):
         client.get(path)
