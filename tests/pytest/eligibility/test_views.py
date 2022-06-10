877631a983792ac2bcee73a9ec3a29ff5e1ecd86
Kegan Maher <kegan@compiler.la>
2022-06-10 08:04:59 +0000

    refactor(tests/eligibility/views): clean up hardcoded routes/templates

--- tests/pytest/eligibility/test_views.py
+++ tests/pytest/eligibility/test_views.py
@@ -6,6 +6,14 @@ from benefits.core import session
 from benefits.eligibility.views import confirm
 
 
+ROUTE_INDEX = "eligibility:index"
+ROUTE_START = "eligibility:start"
+ROUTE_CONFIRM = "eligibility:confirm"
+ROUTE_ENROLLMENT = "enrollment:index"
+TEMPLATE_CONFIRM = "eligibility/confirm.html"
+TEMPLATE_UNVERIFIED = "eligibility/unverified.html"
+
+
 @pytest.fixture
 def mocked_oauth_token_request(mocker, app_request):
     mocker.patch("benefits.core.session.oauth_token", autospec=True, return_value="token")
@@ -22,14 +30,14 @@ def mocked_eligibile_request(mocked_oauth_token_request, mocked_session_eligibil
 @pytest.mark.django_db
 @pytest.mark.usefixtures("mocked_session_agency")
 def test_index_with_agency(client):
-    path = reverse("eligibility:index")
+    path = reverse(ROUTE_INDEX)
     response = client.get(path)
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_index_without_agency(client):
-    path = reverse("eligibility:index")
+    path = reverse(ROUTE_INDEX)
     with pytest.raises(AttributeError, match=r"agency"):
         client.get(path)
 
@@ -37,7 +45,7 @@ def test_index_without_agency(client):
 @pytest.mark.django_db
 @pytest.mark.usefixtures("mocked_session_verifier")
 def test_start_with_verifier(client):
-    path = reverse("eligibility:start")
+    path = reverse(ROUTE_START)
     response = client.get(path)
     assert response.status_code == 200
 
@@ -45,7 +53,7 @@ def test_start_with_verifier(client):
 @pytest.mark.django_db
 @pytest.mark.usefixtures("mocked_session_agency")
 def test_start_without_verifier(client):
-    path = reverse("eligibility:start")
+    path = reverse(ROUTE_START)
     with pytest.raises(AttributeError, match=r"verifier"):
         client.get(path)
 
@@ -56,7 +64,7 @@ def test_confirm_get_unverified(mocked_oauth_token_request):
     response = confirm(mocked_oauth_token_request)
 
     assert response.status_code == 200
-    assert response.template_name == "eligibility/confirm.html"
+    assert response.template_name == TEMPLATE_CONFIRM
 
 
 @pytest.mark.django_db
