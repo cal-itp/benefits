c414e8e78669fcf0d9392250a58a206c87d3010c
Machiko Yasuda <machiko@compiler.la>
2022-06-10 05:59:07 +0000

    test(pytest): testing view when there is only 1 agency with 1 verifier

--- tests/pytest/core/test_views.py
+++ tests/pytest/core/test_views.py
@@ -25,6 +25,23 @@ def test_homepage_single_agency(mocker, client):
 
 
 @pytest.mark.django_db
+def test_homepage_single_agency_single_verifier(mocker, client):
+    # Agency set to DEFTl, which only has 1 verifier
+    agencies = TransitAgency.all_active()[1:]
+    mocker.patch("benefits.core.models.TransitAgency.all_active", return_value=agencies)
+
+    assert len(agencies) == 1
+    assert len(agencies[0].eligibility_verifiers.all()) == 1
+
+    path = reverse("core:index")
+    response = client.get(path)
+
+    assert response.status_code == 302
+    assert response.url == reverse("eligibility:start")
+    # assert response.url == agencies[0].index_url
+
+
+@pytest.mark.django_db
 def test_help(client):
     path = reverse("core:help")
     response = client.get(path)
