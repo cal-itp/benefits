811e1602313acab616104a5c4ceb41162711311d
Kegan Maher <kegan@compiler.la>
2022-06-11 19:46:36 +0000

    test(enrollment): cover success view

--- tests/pytest/enrollment/test_views.py
+++ tests/pytest/enrollment/test_views.py
@@ -7,8 +7,25 @@ import pytest
 
 ROUTE_INDEX = "enrollment:index"
 ROUTE_RETRY = "enrollment:retry"
+ROUTE_SUCCESS = "enrollment:success"
 ROUTE_TOKEN = "enrollment:token"
 TEMPLATE_RETRY = "enrollment/retry.html"
+TEMPLATE_SUCCESS = "enrollment/success.html"
+
+
+@pytest.fixture
+def mocked_session_verifier_auth_required(mocker, first_verifier, mocked_session_verifier):
+    mock_verifier = mocker.Mock(spec=first_verifier)
+    mock_verifier.requires_authentication = True
+    mocked_session_verifier.return_value = mock_verifier
+    return mocked_session_verifier
+
+
+@pytest.fixture
+def mocked_session_verifier_auth_not_required(mocked_session_verifier_auth_required):
+    # mocked_session_verifier_auth_required.return_value is the Mock(spec=first_verifier) from that fixture
+    mocked_session_verifier_auth_required.return_value.requires_authentication = False
+    return mocked_session_verifier_auth_required
 
 
 @pytest.mark.django_db
@@ -105,3 +122,63 @@ def test_retry_valid_form(mocker, client):
 
     assert response.status_code == 200
     assert response.template_name == TEMPLATE_RETRY
+
+
+@pytest.mark.django_db
+def test_success_no_verifier(client):
+    path = reverse(ROUTE_SUCCESS)
+    with pytest.raises(AttributeError, match=r"verifier"):
+        client.get(path)
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("mocked_session_verifier_auth_required")
+def test_success_no_oauth_client(mocker, client):
+    mock_settings = mocker.patch("benefits.enrollment.views.settings")
+    mock_settings.OAUTH_CLIENT_NAME = None
+
+    path = reverse(ROUTE_SUCCESS)
+    with pytest.raises(Exception, match=r"OAUTH_CLIENT_NAME"):
+        client.get(path)
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("mocked_session_verifier_auth_required")
+def test_success_authentication_logged_in(mocker, client):
+    mock_session = mocker.patch("benefits.enrollment.views.session")
+    mock_session.logged_in.return_value = True
+
+    path = reverse(ROUTE_SUCCESS)
+    response = client.get(path)
+
+    assert response.status_code == 200
+    assert response.template_name == TEMPLATE_SUCCESS
+    assert "page" in response.context_data
+    assert "logged-in" in response.context_data["page"].classes
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("mocked_session_verifier_auth_required")
+def test_success_authentication_not_logged_in(mocker, client):
+    mock_session = mocker.patch("benefits.enrollment.views.session")
+    mock_session.logged_in.return_value = False
+
+    path = reverse(ROUTE_SUCCESS)
+    response = client.get(path)
+
+    assert response.status_code == 200
+    assert response.template_name == TEMPLATE_SUCCESS
+    assert "page" in response.context_data
+    assert "logged-out" in response.context_data["page"].classes
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("mocked_session_verifier_auth_not_required")
+def test_success_no_authentication(client):
+    path = reverse(ROUTE_SUCCESS)
+    response = client.get(path)
+
+    assert response.status_code == 200
+    assert response.template_name == TEMPLATE_SUCCESS
+    assert "page" in response.context_data
+    assert "logged-in" not in response.context_data["page"].classes
