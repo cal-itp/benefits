ac952c446714a0e7e719d1bf9588096b599b8337
Kegan Maher <kegan@compiler.la>
2022-06-13 05:54:11 +0000

    test(enrollment): CustomerResponse parsing

--- tests/pytest/enrollment/test_api.py
+++ tests/pytest/enrollment/test_api.py
@@ -2,7 +2,7 @@ import time
 
 import pytest
 
-from benefits.enrollment.api import ApiError, AccessTokenResponse
+from benefits.enrollment.api import ApiError, AccessTokenResponse, CustomerResponse
 
 
 def test_AccessTokenResponse_invalid_response(mocker):
@@ -33,3 +33,51 @@ def test_AccessTokenResponse_valid_response_expires_in(mocker):
     response = AccessTokenResponse(mock_response)
 
     assert response.expiry >= start + expires_in
+
+
+@pytest.mark.parametrize("exception", [KeyError, ValueError])
+def test_CustomerResponse_invalid_response(mocker, exception):
+    mock_response = mocker.Mock()
+    mock_response.json.side_effect = exception
+
+    with pytest.raises(ApiError, match=r"response"):
+        CustomerResponse(mock_response)
+
+
+def test_CustomerResponse_no_id(mocker):
+    mock_response = mocker.Mock()
+    mock_response.json.return_value = {"id": None}
+
+    with pytest.raises(ApiError, match=r"response"):
+        CustomerResponse(mock_response)
+
+
+def test_CustomerResponse(mocker):
+    id = "12345"
+    mock_response = mocker.Mock()
+    mock_response.json.return_value = {"id": id}
+
+    response = CustomerResponse(mock_response)
+
+    assert response.id == id
+    assert not response.is_registered
+
+
+@pytest.mark.parametrize("is_registered", ["true", "True", "tRuE"])
+def test_CustomerResponse_is_registered(mocker, is_registered):
+    mock_response = mocker.Mock()
+    mock_response.json.return_value = {"id": "12345", "is_registered": is_registered}
+
+    response = CustomerResponse(mock_response)
+
+    assert response.is_registered
+
+
+@pytest.mark.parametrize("is_registered", ["false", "Frue", "fAlSe"])
+def test_CustomerResponse_not_is_registered(mocker, is_registered):
+    mock_response = mocker.Mock()
+    mock_response.json.return_value = {"id": "12345", "is_registered": is_registered}
+
+    response = CustomerResponse(mock_response)
+
+    assert not response.is_registered
