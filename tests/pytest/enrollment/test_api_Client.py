f3fcfa47ff985baf8464d52ff8debdcf003bcec4
Kegan Maher <kegan@compiler.la>
2022-06-16 04:22:30 +0000

    refactor(tests): simplify with fixtures

--- tests/pytest/enrollment/test_api_Client.py
+++ tests/pytest/enrollment/test_api_Client.py
@@ -8,6 +8,18 @@ from benefits.enrollment.api import ApiError, CustomerResponse, Client
 REQUESTS_ERRORS = [requests.ConnectionError, requests.Timeout, requests.TooManyRedirects, requests.HTTPError]
 
 
+@pytest.fixture
+def api_client(first_agency):
+    return Client(first_agency)
+
+
+@pytest.fixture
+def mocked_customer(mocker):
+    mock = mocker.Mock(spec=CustomerResponse)
+    mocker.patch("benefits.enrollment.api.CustomerResponse", return_value=mock)
+    return mock
+
+
 def test_init_no_agency():
     with pytest.raises(ValueError, match=r"agency"):
         Client(None)
@@ -33,102 +45,84 @@ def test_init(mocker):
 
 @pytest.mark.django_db
 @pytest.mark.parametrize("exception", REQUESTS_ERRORS)
-def test_access_token_exception(mocker, first_agency, exception):
-    client = Client(first_agency)
+def test_access_token_exception(mocker, api_client, exception):
     mock_response = mocker.Mock()
     mock_response.raise_for_status.side_effect = exception
-    mocker.patch.object(client, "_post", return_value=mock_response)
+    mocker.patch.object(api_client, "_post", return_value=mock_response)
 
     with pytest.raises(ApiError):
-        client.access_token()
+        api_client.access_token()
 
 
 @pytest.mark.django_db
-def test_access_token(mocker, first_agency):
-    client = Client(first_agency)
+def test_access_token(mocker, api_client):
     mock_response = mocker.Mock()
-    mocker.patch.object(client, "_post", return_value=mock_response)
+    mocker.patch.object(api_client, "_post", return_value=mock_response)
     mocker.patch("benefits.enrollment.api.AccessTokenResponse")
 
-    token = client.access_token()
+    token = api_client.access_token()
 
     assert token
 
 
 @pytest.mark.django_db
-def test_enroll_no_customer_token(first_agency):
-    client = Client(first_agency)
-
+def test_enroll_no_customer_token(api_client):
     with pytest.raises(ValueError, match=r"customer_token"):
-        client.enroll(None, "group_id")
+        api_client.enroll(None, "group_id")
 
 
 @pytest.mark.django_db
-def test_enroll_no_group_id(first_agency):
-    client = Client(first_agency)
-
+def test_enroll_no_group_id(api_client):
     with pytest.raises(ValueError, match=r"group_id"):
-        client.enroll("customer_token", None)
+        api_client.enroll("customer_token", None)
 
 
 @pytest.mark.django_db
-def test_get_customer_no_token(first_agency):
-    client = Client(first_agency)
-
+def test_get_customer_no_token(api_client):
     with pytest.raises(ValueError, match=r"token"):
-        client._get_customer(None)
+        api_client._get_customer(None)
 
 
 @pytest.mark.django_db
-def test_get_customer_status_not_OK(mocker, first_agency):
-    client = Client(first_agency)
+def test_get_customer_status_not_OK(mocker, api_client):
     mock_response = mocker.Mock()
     mock_response.raise_for_status.side_effect = requests.HTTPError()
-    mocker.patch.object(client, "_get", return_value=mock_response)
+    mocker.patch.object(api_client, "_get", return_value=mock_response)
 
     with pytest.raises(ApiError):
-        client._get_customer("token")
+        api_client._get_customer("token")
 
 
 @pytest.mark.django_db
-def test_get_customer_is_registered(mocker, first_agency):
-    client = Client(first_agency)
+def test_get_customer_is_registered(mocker, api_client, mocked_customer):
     mock_get_response = mocker.Mock()
-    mocker.patch.object(client, "_get", return_value=mock_get_response)
+    mocker.patch.object(api_client, "_get", return_value=mock_get_response)
+    mocked_customer.is_registered = True
 
-    mock_customer = mocker.Mock(spec=CustomerResponse)
-    mock_customer.is_registered = True
-    mocker.patch("benefits.enrollment.api.CustomerResponse", return_value=mock_customer)
+    return_customer = api_client._get_customer("token")
 
-    return_customer = client._get_customer("token")
-
-    assert return_customer == mock_customer
+    assert return_customer == mocked_customer
     assert return_customer.is_registered
 
 
 @pytest.mark.django_db
-def test_get_customer_is_not_registered(mocker, first_agency):
-    client = Client(first_agency)
+def test_get_customer_is_not_registered(mocker, api_client, mocked_customer):
     mock_get_response = mocker.Mock()
-    mocker.patch.object(client, "_get", return_value=mock_get_response)
-
-    mock_customer = mocker.Mock(spec=CustomerResponse)
-    mock_customer.is_registered = False
-    mock_customer.id = "id"
-    mocker.patch("benefits.enrollment.api.CustomerResponse", return_value=mock_customer)
+    mocker.patch.object(api_client, "_get", return_value=mock_get_response)
+    mocked_customer.is_registered = False
+    mocked_customer.id = "id"
 
     update_spy = mocker.patch("benefits.enrollment.api.Client._update_customer")
 
-    client._get_customer("token")
+    api_client._get_customer("token")
 
-    update_spy.assert_called_once_with(mock_customer.id)
+    update_spy.assert_called_once_with(mocked_customer.id)
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize("exception", REQUESTS_ERRORS)
-def test_get_customer_exception(mocker, first_agency, exception):
-    client = Client(first_agency)
-    mocker.patch.object(client, "_get", side_effect=exception)
+def test_get_customer_exception(mocker, api_client, exception):
+    mocker.patch.object(api_client, "_get", side_effect=exception)
 
     with pytest.raises(ApiError):
-        client._get_customer("token")
+        api_client._get_customer("token")
