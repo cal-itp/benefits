d7f5348898246a89562659531324544e8d8f04c3
Angela Tran <angela@compiler.la>
2022-06-16 20:50:12 +0000

    refactor(tests): use new mocked_oauth_client_instance fixture

--- tests/pytest/oauth/test_views.py
+++ tests/pytest/oauth/test_views.py
@@ -74,11 +74,12 @@ def test_authorize_success(mocked_oauth_client_instance, mocked_analytics_module
 
 
 @pytest.mark.django_db
-def test_authorize_success_with_claim(mocked_session_verifier_auth_required, mocked_oauth_client, app_request):
+def test_authorize_success_with_claim(mocked_session_verifier_auth_required, mocked_oauth_client_instance, app_request):
     # mocked_session_verifier_auth_required is a fixture that mocks benefits.core.session.verifier(request)
     # call it here, passing a None request, to get the return value from the mock
     verifier = mocked_session_verifier_auth_required(None)
     verifier.auth_claim = "claim"
+    mocked_oauth_client = mocked_oauth_client_instance.return_value
     mocked_oauth_client.authorize_access_token.return_value = {"id_token": "token", "userinfo": {"claim": "True"}}
 
     result = authorize(app_request)
@@ -90,11 +91,12 @@ def test_authorize_success_with_claim(mocked_session_verifier_auth_required, moc
 
 
 @pytest.mark.django_db
-def test_authorize_success_without_claim(mocked_session_verifier_auth_required, mocked_oauth_client, app_request):
+def test_authorize_success_without_claim(mocked_session_verifier_auth_required, mocked_oauth_client_instance, app_request):
     # mocked_session_verifier_auth_required is a fixture that mocks benefits.core.session.verifier(request)
     # call it here, passing a None request, to get the return value from the mock
     verifier = mocked_session_verifier_auth_required(None)
     verifier.auth_claim = ""
+    mocked_oauth_client = mocked_oauth_client_instance.return_value
     mocked_oauth_client.authorize_access_token.return_value = {"id_token": "token", "userinfo": {"claim": "True"}}
 
     result = authorize(app_request)
