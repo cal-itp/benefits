b48aec1c4fe71fa0fc850ecf72a5d542898824fc
Kegan Maher <kegan@compiler.la>
2022-06-17 05:04:08 +0000

    feat(models): add OAuth config fields to AuthProvider

--- benefits/core/models.py
+++ benefits/core/models.py
@@ -29,6 +29,9 @@ class AuthProvider(models.Model):
     id = models.AutoField(primary_key=True)
     sign_in_button_label = models.TextField()
     sign_out_button_label = models.TextField()
+    client_name = models.TextField()
+    client_id = models.TextField()
+    authority = models.TextField()
     scope = models.TextField(null=True)
     claim = models.TextField(null=True)
 
