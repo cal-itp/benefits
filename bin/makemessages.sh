93035e1e1a7447841eb9171b8a8acd14497ad362
Angela Tran <angela@compiler.la>
2022-07-12 19:54:57 +0000

    refactor: regenerate PO files using helper script
    this gets us in a state where any messages not present in Django
    templates or views are commented out, and from here, we can clean up
    entries that are actually unused vs. in the fixtures.

---
+++ bin/makemessages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eu
+
+# generate initial .PO files from msgids in template and view files
+
+python manage.py makemessages -a
+
+# put back bug-report link
+sed -i 's/Report-Msgid-Bugs-To\:/Report-Msgid-Bugs-To\: https\:\/\/github.com\/cal-itp\/benefits\/issues/g' benefits/locale/**/**/**.po
