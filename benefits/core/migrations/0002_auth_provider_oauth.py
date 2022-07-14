031a9150a36a4c7a7b4d9f7530bd3a294dae66f5
Kegan Maher <kegan@compiler.la>
2022-07-14 22:32:13 +0000

    feat: create a data migration for OAuth AuthProvider

---
+++ benefits/core/migrations/0002_auth_provider_oauth.py
@@ -0,0 +1,27 @@
+from django.db import migrations
+from django.utils.translation import gettext_lazy as _
+
+
+def create_model(app, *args, **kwargs):
+    AuthProvider = app.get_model("core", "AuthProvider")
+
+    AuthProvider.objects.create(
+        sign_in_button_label=_("eligibility.buttons.signin"),
+        sign_out_button_label=_("eligibility.buttons.signout"),
+        client_name="benefits-oauth-client-name",
+        client_id="benefits-oauth-client-id",
+        authority="https://example.com",
+        scope="verify:type1",
+        claim="type1",
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_model),
+    ]
