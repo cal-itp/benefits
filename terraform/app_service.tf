2f769a58abdbc6797492cb6fd88bb3cdc9192182
Aidan Feldman <aidan@compiler.la>
2022-07-19 12:57:54 -0400

    chore: test change to slot
    This is to confirm that our service connection in the pipeline has sufficient permissions to change the App Service slots. Will be reverted after merge and successful deploy.

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -84,7 +84,7 @@ resource "azurerm_linux_web_app_slot" "dev" {
   app_service_id = azurerm_linux_web_app.main.id
 
   site_config {
-    ftps_state             = "Disabled"
+    ftps_state             = "FtpsOnly"
     vnet_route_all_enabled = true
   }
 
