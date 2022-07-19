08c6f646ed4c00bcedd15ceca59b8aa65d8048a0
Kegan Maher <kegan@compiler.la>
2022-07-19 21:43:49 +0000

    test(EligibilityVerifier): is_auth_required

--- tests/pytest/core/test_models.py
+++ tests/pytest/core/test_models.py
@@ -2,16 +2,22 @@ import pytest
 
 
 @pytest.mark.django_db
-def test_EligibilityVerifier_with_auth_verification(model_EligibilityVerifier, model_AuthProvider_with_verification):
+def test_EligibilityVerifier_with_AuthProvider_with_verification(
+    model_EligibilityVerifier, model_AuthProvider_with_verification
+):
     model_EligibilityVerifier.auth_provider = model_AuthProvider_with_verification
 
+    assert model_EligibilityVerifier.is_auth_required
     assert model_EligibilityVerifier.uses_auth_verification
 
 
 @pytest.mark.django_db
-def test_EligibilityVerifier_without_auth_verification(model_EligibilityVerifier, model_AuthProvider_without_verification):
+def test_EligibilityVerifier_with_AuthProvider_without_verification(
+    model_EligibilityVerifier, model_AuthProvider_without_verification
+):
     model_EligibilityVerifier.auth_provider = model_AuthProvider_without_verification
 
+    assert model_EligibilityVerifier.is_auth_required
     assert not model_EligibilityVerifier.uses_auth_verification
 
 
@@ -19,4 +25,5 @@ def test_EligibilityVerifier_without_auth_verification(model_EligibilityVerifier
 def test_EligibilityVerifier_without_AuthProvider(model_EligibilityVerifier):
     model_EligibilityVerifier.auth_provider = None
 
+    assert not model_EligibilityVerifier.is_auth_required
     assert not model_EligibilityVerifier.uses_auth_verification
