d7ab2b6511171cde09831cb11338abc3d13ebe69
Kegan Maher <kegan@compiler.la>
2022-07-19 21:59:18 +0000

    test(EligibilityType): get_many

--- tests/pytest/core/test_models.py
+++ tests/pytest/core/test_models.py
@@ -17,6 +17,34 @@ def test_EligibilityType_get_nonmatching():
 
 
 @pytest.mark.django_db
+def test_EligibilityType_get_many_matching(model_EligibilityType):
+    new_type = EligibilityType.get(model_EligibilityType.id)
+    new_type.pk = None
+    new_type.save()
+
+    result = EligibilityType.get_many([model_EligibilityType.id, new_type.id])
+
+    assert len(result) == 2
+    assert model_EligibilityType in result
+    assert new_type in result
+
+
+@pytest.mark.django_db
+def test_EligibilityType_get_many_nonmatching():
+    result = EligibilityType.get_many([99998, 99999])
+
+    assert len(result) == 0
+
+
+@pytest.mark.django_db
+def test_EligibilityType_get_many_somematching(model_EligibilityType):
+    result = EligibilityType.get_many([model_EligibilityType.id, 99999])
+
+    assert len(result) == 1
+    assert model_EligibilityType in result
+
+
+@pytest.mark.django_db
 def test_EligibilityVerifier_by_id_matching(model_EligibilityVerifier):
     verifier = EligibilityVerifier.by_id(model_EligibilityVerifier.id)
 
