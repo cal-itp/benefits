8f6d7c67be347cea3b15cd7110d184b848ebe56f
Aidan Feldman <aidan@compiler.la>
2022-07-21 18:06:30 -0400

    chore: remove App Service Environment
    We aren't able to create it anyway. Will try again later.

--- terraform/network.tf
+++ terraform/network.tf
@@ -8,20 +8,3 @@ data "azurerm_subnet" "main" {
   virtual_network_name = "VNET-CDT-PUB-SHRD-W-P-001"
   resource_group_name  = local.network_resource_group_name
 }
-
-resource "azurerm_app_service_environment_v3" "main" {
-  name                = "ASE-CDT-PUB-CALITP-P-001"
-  resource_group_name = local.network_resource_group_name
-  subnet_id           = data.azurerm_subnet.main.id
-
-  # override the default Cipher suite as a remediation to vulnerability scanning
-  # https://github.com/cal-itp/benefits/issues/771
-  cluster_setting {
-    name  = "FrontEndSSLCipherSuiteOrder"
-    value = "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
-  }
-
-  lifecycle {
-    ignore_changes = [tags]
-  }
-}
