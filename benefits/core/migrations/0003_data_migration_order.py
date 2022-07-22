fbdbf3059c0375acaf889d32532e70f8a56a25d2
Angela Tran <angela@compiler.la>
2022-07-22 20:42:56 +0000

    refactor(migration): clean up path-building

--- benefits/core/migrations/0003_data_migration_order.py
+++ benefits/core/migrations/0003_data_migration_order.py
@@ -9,7 +9,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    migrations_directory = settings.BASE_DIR + "/benefits/core/migrations"
+    migrations_directory = os.path.join(settings.BASE_DIR, "benefits", "core", "migrations")
     dependencies = [("core", file.replace(".py", "")) for file in os.listdir(migrations_directory) if file.startswith("0002")]
 
     operations = []
