3311a8a10c3fd66bec94a4b3850b1fe77dd92c61
Angela Tran <angela@compiler.la>
2022-07-22 20:03:42 +0000

    fix: migration helper script needs to account for data migrations
    update models and migration docs

--- bin/makemigrations.sh
+++ bin/makemigrations.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 set -eux
 
-# remove existing migration file
+# copy old migrations to temporary directory
 
-rm -f benefits/core/migrations/0001_initial.py
+cp -r benefits/core/migrations benefits/core/old_migrations
 
 # regenerate
 
 python manage.py makemigrations
+
+# copy over migrations that don't exist
+
+cp benefits/core/old_migrations/* benefits/core/migrations --no-clobber --recursive
+
+# clean up temporary directory
+rm -rf benefits/core/old_migrations
 
 # reformat with black
 
