060cccea617136270625c85bc7fd506cd7712f42
Machiko Yasuda <machiko@compiler.la>
2022-08-23 22:07:21 +0000

    refactor(analytics): use a generalized event instead with parameters for success/fail

--- benefits/enrollment/analytics.py
+++ benefits/enrollment/analytics.py
@@ -4,13 +4,15 @@ The enrollment application: analytics implementation.
 from benefits.core import analytics as core
 
 
-class CompletedEnrollment(core.Event):
-    """Analytics event representing a successfully completed payment processor enrollment."""
+class ReturnedEnrollmentEvent(core.Event):
+    """Analytics event representing the end of payment processor enrollment request."""
 
-    def __init__(self, request):
-        super().__init__(request, "completed enrollment")
+    def __init__(self, request, status, error=None):
+        super().__init__(request, "returned enrollment")
+        if str(status).lower() in ("error", "fail", "success"):
+            self.update_event_properties(status=status, error=error)
 
 
-def completed_enrollment(request):
+def returned_success(request):
     """Send the "completed enrollment" analytics event."""
-    core.send_event(CompletedEnrollment(request))
+    core.send_event(ReturnedEnrollmentEvent(request, status="success"))
