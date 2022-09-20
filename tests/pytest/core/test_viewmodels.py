b7de63aa6685365eb2144dd15bbda703b2e64d90
Angela Tran <angela@compiler.la>
2022-09-20 19:07:23 +0000

    refactor(tests): parameterize error page translation test
    add translation strings and coverage for ErrorPage.user_error

--- tests/pytest/core/test_viewmodels.py
+++ tests/pytest/core/test_viewmodels.py
@@ -1,16 +1,43 @@
 from django.utils import translation
 from django.utils.translation import gettext as _
+import pytest
 
 from benefits.core.viewmodels import ErrorPage
 
 
-def test_ErrorPage_server_error_translations():
+@pytest.mark.parametrize(
+    "error_page_function,title,content_title,paragraphs",
+    [
+        pytest.param(
+            ErrorPage.server_error,
+            "core.pages.server_error.title",
+            "core.pages.server_error.title",
+            ["core.pages.server_error.p[0]", "core.pages.server_error.p[1]"],
+            id="server_error",
+        ),
+        pytest.param(
+            ErrorPage.not_found,
+            "core.pages.not_found.title",
+            "core.pages.not_found.content_title",
+            ["core.pages.not_found.p[0]"],
+            id="not_found",
+        ),
+        pytest.param(
+            ErrorPage.user_error,
+            "core.pages.user_error.title",
+            "core.pages.user_error.content_title",
+            ["core.pages.user_error.p[0]"],
+            id="user_error",
+        ),
+    ],
+)
+def test_ErrorPage_translations(error_page_function, title, content_title, paragraphs):
     translation.activate("en")
 
-    error_page = ErrorPage.server_error()
-    english_title = f"{_('core.pages.index.prefix')}: {_('core.pages.server_error.title')}"
-    english_content_title = _("core.pages.server_error.title")
-    english_paragraphs = [_("core.pages.server_error.p[0]"), _("core.pages.server_error.p[1]")]
+    error_page = error_page_function()
+    english_title = f"{_('core.pages.index.prefix')}: {_(title)}"
+    english_content_title = _(content_title)
+    english_paragraphs = [_(paragraph) for paragraph in paragraphs]
 
     assert error_page.title == english_title
     assert error_page.content_title == english_content_title
@@ -18,38 +45,10 @@ def test_ErrorPage_server_error_translations():
 
     translation.activate("es")
 
-    error_page = ErrorPage.server_error()
-    spanish_title = f"{_('core.pages.index.prefix')}: {_('core.pages.server_error.title')}"
-    spanish_content_title = _("core.pages.server_error.title")
-    spanish_paragraphs = [_("core.pages.server_error.p[0]"), _("core.pages.server_error.p[1]")]
-
-    assert error_page.title != english_title
-    assert error_page.content_title != english_content_title
-    assert error_page.paragraphs != english_paragraphs
-
-    assert error_page.title == spanish_title
-    assert error_page.content_title == spanish_content_title
-    assert error_page.paragraphs == spanish_paragraphs
-
-
-def test_ErrorPage_not_found_translations():
-    translation.activate("en")
-
-    error_page = ErrorPage.not_found()
-    english_title = f"{_('core.pages.index.prefix')}: {_('core.pages.not_found.title')}"
-    english_content_title = _("core.pages.not_found.content_title")
-    english_paragraphs = [_("core.pages.not_found.p[0]")]
-
-    assert error_page.title == english_title
-    assert error_page.content_title == english_content_title
-    assert error_page.paragraphs == english_paragraphs
-
-    translation.activate("es")
-
-    error_page = ErrorPage.not_found()
-    spanish_title = f"{_('core.pages.index.prefix')}: {_('core.pages.not_found.title')}"
-    spanish_content_title = _("core.pages.not_found.content_title")
-    spanish_paragraphs = [_("core.pages.not_found.p[0]")]
+    error_page = error_page_function()
+    spanish_title = f"{_('core.pages.index.prefix')}: {_(title)}"
+    spanish_content_title = _(content_title)
+    spanish_paragraphs = [_(paragraph) for paragraph in paragraphs]
 
     assert error_page.title != english_title
     assert error_page.content_title != english_content_title
