e259b1ff628bb26e71bacdde1c617ddcd0976a94
Aidan Feldman <aidan@compiler.la>
2022-10-27 14:41:41 -0400

    chore(Terraform): remove Slack alert setup - REVERT ME
    Running into permissions issues creating the Secret.

--- terraform/monitor.tf
+++ terraform/monitor.tf
@@ -22,20 +22,10 @@ resource "azurerm_application_insights" "main" {
   }
 }
 
-data "azurerm_key_vault_secret" "slack_benefits_notify_email" {
-  name         = "slack-benefits-notify-email"
-  key_vault_id = azurerm_key_vault.main.id
-}
-
 resource "azurerm_monitor_action_group" "eng_email" {
   name                = "benefits-notify Slack channel email"
   resource_group_name = data.azurerm_resource_group.main.name
   short_name          = "slack-notify"
-
-  email_receiver {
-    name          = "Benefits engineering team"
-    email_address = data.azurerm_key_vault_secret.slack_benefits_notify_email.value
-  }
 
   lifecycle {
     ignore_changes = [tags]
