b1e4a62e0971ed5213996fc7bbde224b481ae1a3
Kegan Maher <kegan@compiler.la>
2023-01-31 20:59:17 +0000

    refactor(tests): remove duplicate test
    now all EligibiltyEvents overwrite the user properties as well

--- tests/pytest/eligibility/test_analytics.py
+++ tests/pytest/eligibility/test_analytics.py
@@ -1,6 +1,6 @@
 import pytest
 
-from benefits.eligibility.analytics import EligibilityEvent, ReturnedEligibilityEvent
+from benefits.eligibility.analytics import EligibilityEvent
 
 
 @pytest.mark.django_db
@@ -20,17 +20,3 @@ def test_EligibilityEvent_overwrites_eligibility_types(app_request, mocker):
     assert key in event.user_properties
     assert type2 in event.user_properties[key]
     assert type1 not in event.user_properties[key]
-
-
-@pytest.mark.django_db
-def test_ReturnedEligibilityEvent_success_overwrites_user_eligibility_types(app_request, mocker):
-    key, type1, type2 = "eligibility_types", "type1", "type2"
-    mocker.patch("benefits.core.analytics.session.eligibility", return_value=[type1])
-    mocker.patch("benefits.core.analytics.EligibilityType.get_names", return_value=[type1])
-
-    event = ReturnedEligibilityEvent(app_request, eligibility_types=[type2], status="success")
-
-    # user_properties should have been overwritten
-    assert key in event.user_properties
-    assert type1 not in event.user_properties[key]
-    assert type2 in event.user_properties[key]
