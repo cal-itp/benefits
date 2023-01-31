1e4b515b3f92ff832458fb1c0883da925c6b14e5
Kegan Maher <kegan@compiler.la>
2023-01-31 18:06:24 +0000

    fix(eligibility): update user props in events
    so user-based charts also have this data

--- tests/pytest/eligibility/test_analytics.py
+++ tests/pytest/eligibility/test_analytics.py
@@ -4,7 +4,7 @@ from benefits.eligibility.analytics import EligibilityEvent, ReturnedEligibility
 
 
 @pytest.mark.django_db
-def test_EligibilityEvent_overwrites_event_eligibility_types(app_request, mocker):
+def test_EligibilityEvent_overwrites_eligibility_types(app_request, mocker):
     key, type1, type2 = "eligibility_types", "type1", "type2"
     mocker.patch("benefits.core.analytics.session.eligibility", return_value=[type1])
     mocker.patch("benefits.core.analytics.EligibilityType.get_names", return_value=[type1])
@@ -16,10 +16,10 @@ def test_EligibilityEvent_overwrites_event_eligibility_types(app_request, mocker
     assert type2 in event.event_properties[key]
     assert type1 not in event.event_properties[key]
 
-    # user_properties should not have been overwritten
+    # user_properties should have been overwritten
     assert key in event.user_properties
-    assert type1 in event.user_properties[key]
-    assert type2 not in event.user_properties[key]
+    assert type2 in event.user_properties[key]
+    assert type1 not in event.user_properties[key]
 
 
 @pytest.mark.django_db
