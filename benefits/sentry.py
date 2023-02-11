d43dd7df7b1d9c99cdedc7e4f41a410265bd5f30
Aidan Feldman <aidan.feldman@gmail.com>
2023-02-11 17:49:24 +0000

    chore(Sentry): use the Git SHA on local and dev

--- benefits/sentry.py
+++ benefits/sentry.py
@@ -2,14 +2,39 @@ from benefits import VERSION
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 import os
+import subprocess
+
+
+SENTRY_ENVIRONMENT = os.environ.get("SENTRY_ENVIRONMENT", "local")
+
+
+# https://stackoverflow.com/a/21901260/358804
+def get_git_revision_hash():
+    return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
+
+
+def get_sha_path():
+    current_file = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_file, "..", "static", "sha.txt")
+
+
+def get_release() -> str:
+    """On 'local' and 'dev', returns the Git SHA. Otherwise, returns the VERSION."""
+
+    sha_path = get_sha_path()
+    if SENTRY_ENVIRONMENT == "local":
+        return get_git_revision_hash()
+    elif SENTRY_ENVIRONMENT == "dev" and os.path.isfile(sha_path):
+        with open(sha_path) as f:
+            return f.read().strip()
+    else:
+        return VERSION
 
 
 def configure():
     SENTRY_DSN = os.environ.get("SENTRY_DSN")
     if SENTRY_DSN:
         print("Enabling Sentry…")
-
-        SENTRY_ENVIRONMENT = os.environ.get("SENTRY_ENVIRONMENT", "local")
 
         # https://docs.sentry.io/platforms/python/configuration/
         sentry_sdk.init(
@@ -19,7 +44,7 @@ def configure():
             ],
             traces_sample_rate=1.0,
             environment=SENTRY_ENVIRONMENT,
-            release=VERSION,
+            release=get_release(),
         )
     else:
         print("SENTRY_DSN not set, so won't send events")
