9b9a3412095aea5dd393d344f64e1da375779117
Aidan Feldman <aidan.feldman@gmail.com>
2023-02-16 02:59:16 +0000

    chore(Cypress): delete UI tests that aren't useful
    Per discussion amongst the engineering team, we should not bother maintaining tests that only check whether text is present.

--- tests/cypress/specs/ui/help.cy.js
+++ tests/cypress/specs/ui/help.cy.js
@@ -1,22 +1,6 @@
-const agencies = require("../../fixtures/transit-agencies");
-
 describe("Help page spec", () => {
   beforeEach(() => {
     cy.visit("/");
-  });
-
-  it("Clicking on Help takes user to Help", () => {
-    cy.contains("Help").click();
-
-    cy.location("pathname").should("eq", "/help");
-  });
-
-  it("Contains a back link", () => {
-    cy.contains("Help").click();
-
-    cy.contains("Go back").then(($e) => {
-      expect($e).attr("href").eql("/");
-    });
   });
 
   it("Allows user to go back", () => {
@@ -27,43 +11,11 @@ describe("Help page spec", () => {
     cy.location("pathname").should("eq", "/");
   });
 
-  it("Has help information for all transit agencies", () => {
-    cy.contains("Help").click();
-
-    agencies.forEach(function (agency) {
-      cy.contains(agency.fields.long_name);
-      cy.contains(agency.fields.phone);
-    });
-  });
-
-  it("Has help information for correct transit agency if clicking Help from a transit page", () => {
-    let chosenAgency = agencies[0];
-    let otherAgency = agencies[1];
-    cy.contains(chosenAgency.fields.short_name).click();
-    cy.contains("Help").click();
-
-    cy.contains(chosenAgency.fields.long_name);
-    cy.contains(chosenAgency.fields.phone);
-    cy.should("not.contain", otherAgency.fields.long_name);
-    cy.should("not.contain", otherAgency.fields.phone);
-  });
-
   it("Has headers that all have ids to be used for anchor links", () => {
     cy.contains("Help").click();
 
     cy.get("h2").each(($el, index, $list) => {
       cy.wrap($el[0].attributes).its("id").should("exist");
     });
-  });
-
-  it("Has the expected headers", () => {
-    cy.contains("Help").click();
-
-    cy.get("h2").contains("What is Cal-ITP Benefits?");
-    cy.get("h2").contains("Payment options");
-    cy.get("h2").contains("What is Login.gov?");
-    cy.get("h2").contains("How do I verify my identity on Login.gov?");
-    cy.get("h2").contains("What is Littlepay?");
-    cy.get("h2").contains("Questions?");
   });
 });
