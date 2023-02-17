d5a4f2713e0b9b7166f0960323275aa29fbfc7f3
Angela Tran <angela@compiler.la>
2023-02-17 20:20:31 +0000

    fix: adapt test to changes in dev

--- tests/pytest/core/test_middleware_healthcheck_user_agent.py
+++ tests/pytest/core/test_middleware_healthcheck_user_agent.py
@@ -4,8 +4,7 @@ from django.urls import reverse
 import pytest
 
 from benefits.core import session
-from benefits.core.models import TransitAgency
-from benefits.core.views import ROUTE_INDEX, TEMPLATE_PAGE
+from benefits.core.views import ROUTE_INDEX, TEMPLATE_INDEX
 
 
 @pytest.mark.django_db
@@ -21,16 +20,10 @@ def test_healthcheck_user_agent(mocker, user_agent):
 
 
 @pytest.mark.django_db
-def test_non_healthcheck_user_agent(mocker, model_TransitAgency, client):
-    # create another Transit Agency by cloning the original to ensure there are multiple
-    # https://stackoverflow.com/a/48149675/453168
-    new_agency = TransitAgency.objects.get(pk=model_TransitAgency.id)
-    new_agency.pk = None
-    new_agency.save()
-
+def test_non_healthcheck_user_agent(mocker, client):
     mocker.patch.object(session.settings, "HEALTHCHECK_USER_AGENTS", ["AlwaysOn"])
 
     response = client.get(reverse(ROUTE_INDEX))
 
     assert response.status_code == 200
-    assert response.template_name == TEMPLATE_PAGE
+    assert response.template_name == TEMPLATE_INDEX
