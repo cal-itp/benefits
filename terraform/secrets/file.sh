657198d76405d2e7057808922ccdb803bd4c30f6
Angela Tran <angela@compiler.la>
2023-02-24 01:58:52 +0000

    refactor: move argument check above variable assignment

--- terraform/secrets/file.sh
+++ terraform/secrets/file.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-env=$1
-secret_name=$2
-file_path=$3
-
 if [ $# -ne 3 ]; then
   echo "Usage: $0 <D|T|P> <secret_name> <file_path>"
   exit 1
 fi
+
+env=$1
+secret_name=$2
+file_path=$3
 
 az keyvault secret set --vault-name "KV-CDT-PUB-CALITP-$env-001" --name $1 --file $2
