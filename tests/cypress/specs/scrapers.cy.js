1f33670b58dcb0498e9e70dde7fbc9b21e4a1b29
Kegan Maher <kegan@compiler.la>
2023-04-14 16:28:50 -0700

    fix(tests): simplify, ensure nginx throws 404

--- tests/cypress/specs/scrapers.cy.js
+++ tests/cypress/specs/scrapers.cy.js
@@ -1,5 +1,6 @@
 const endpoints = ["auth", "cgi", "eligibility/app", "login", "sample/api"];
 const files = [".env", "wp-admin/login.php", "data.json", "secrets/prod.yaml"];
+const targets = endpoints.concat(files);
 
 const visit = (partial_path) => {
   return cy.request({
@@ -13,18 +14,12 @@ const visit = (partial_path) => {
 const NOT_FOUND = 404;
 
 describe("Scraper filtering spec", () => {
-  endpoints.forEach((endpoint) => {
-    it(`404s known scraper endpoint pattern: /${endpoint}`, () => {
-      visit(endpoint).then((res) => {
+  targets.forEach((target) => {
+    it(`nginx returns 404 for known scraper target pattern: /${target}`, () => {
+      visit(target).then((res) => {
         expect(res.status).to.eq(NOT_FOUND);
-      });
-    });
-  });
-
-  files.forEach((file) => {
-    it(`404s known scraper file pattern: /${file}`, () => {
-      visit(file).then((res) => {
-        expect(res.status).to.eq(NOT_FOUND);
+        expect(res.body).to.contain("nginx");
+        expect(res.body).not.to.contain("Cal-ITP");
       });
     });
   });
