a3a6896ecc40b120f4b04f5f89bf7d24de9762fb
Angela Tran <angela@compiler.la>
2023-06-28 22:25:28 +0000

    refactor: remove sign out label from veteran AuthProvider

--- benefits/core/models.py
+++ benefits/core/models.py
@@ -43,7 +43,7 @@ class AuthProvider(models.Model):
 
     id = models.AutoField(primary_key=True)
     sign_in_button_label = models.TextField()
-    sign_out_button_label = models.TextField()
+    sign_out_button_label = models.TextField(null=True)
     client_name = models.TextField()
     client_id = models.TextField()
     authority = models.TextField()
