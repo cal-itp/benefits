35bb5909a34cd9992420e29b3bac8c1d37d76f33
Kegan Maher <kegan@compiler.la>
2023-07-06 22:32:00 +0000

    refactor: no need for bankcardcheck tag, use include

--- benefits/eligibility/templatetags/eligibility.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-
-register = template.Library()
-
-
-@register.inclusion_tag("eligibility/tags/media_item__start__bankcardcheck.html")
-def media_item__start__bankcardcheck():
-    return {}
