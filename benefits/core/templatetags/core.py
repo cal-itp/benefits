bbd95abc9e3bc4817e1355d2be1e8f7349b06301
Kegan Maher <kegan@compiler.la>
2023-07-06 06:40:42 +0000

    feat: generic button tag
    opens/closes for arbitrary inner content

--- benefits/core/templatetags/core.py
+++ benefits/core/templatetags/core.py
@@ -64,3 +64,60 @@ class MediaItemNode(template.Node):
         # render the tag template
         t = context.template.engine.get_template("core/tags/media_item.html")
         return t.render(ctx)
+
+
+@register.tag
+def button(parser, token):
+    """
+    Defines the compile function for the tag `{% button %}{% endbutton %}`
+    """
+    # token.contents is the tag name + all arguments
+    # "button url='/url' classes='cls' id=variable"
+    # split and discard the first (tag name)
+    token_contents = token.split_contents()[1:]
+    # the arg keys supported by this tag
+    args = ["url", "classes", "id", "target", "rel", "role"]
+    # token_contents are string "key=value" pairs, convert to dict
+    kwargs = {}
+    for token_arg in token_contents:
+        k, v = token_arg.split("=")
+        if k in args:
+            kwargs[k] = v
+
+    # parse the rest of the tag contents
+    nodelist = parser.parse(("endbutton",))
+    # "consume" the end tag
+    # https://docs.djangoproject.com/en/4.2/howto/custom-template-tags/#parsing-until-another-block-tag
+    parser.delete_first_token()
+    return ButtonNodeItem(nodelist, args, **kwargs)
+
+
+class ButtonNodeItem(template.Node):
+    """
+    Template Node representing an instance of a tag `{% button url="" classes="" %}{% endbutton %}`.
+    """
+
+    def __init__(self, nodelist, arg_names, **kwargs):
+        self.nodelist = nodelist
+        self.arg_names = arg_names
+        for name in arg_names:
+            val = kwargs.get(name)
+            if val:
+                setattr(self, name, template.Variable(val))
+
+    def render(self, context):
+        # render the nodelist (tag inner content) into a new "body" context
+        ctx = template.Context({"body": self.nodelist.render(context)}, autoescape=context.autoescape)
+        # add the tag arguments to context, .resolve() each template.Variable
+        for arg in self.arg_names:
+            try:
+                ctx[arg] = getattr(self, arg).resolve(context)
+            except AttributeError:
+                pass
+        # adjust classes to list as-needed
+        classes = ctx.get("classes")
+        if isinstance(classes, str):
+            ctx["classes"] = classes.split(" ")
+        # render the tag template
+        t = context.template.engine.get_template("core/tags/button.html")
+        return t.render(ctx)
