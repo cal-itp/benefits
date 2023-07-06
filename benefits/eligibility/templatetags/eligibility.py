2d6c20ca94990f83d42a1d3c4977ba329e8fb137
Kegan Maher <kegan@compiler.la>
2023-07-06 04:04:53 +0000

    feat: bankcardcheck media item
    move hardcoding to template

--- benefits/eligibility/templatetags/eligibility.py
+++ benefits/eligibility/templatetags/eligibility.py
@@ -4,6 +4,11 @@ from django import template
 register = template.Library()
 
 
+@register.inclusion_tag("eligibility/tags/media_item__bankcardcheck.html")
+def media_item__bankcardcheck():
+    return {}
+
+
 @register.inclusion_tag("eligibility/tags/media_item__idcardcheck__mst_cc.html")
 def media_item__idcardcheck__mst_cc():
     return {}
