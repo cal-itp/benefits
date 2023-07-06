86258a6021dc51070f4749d9d09a67d70f8f8210
Kegan Maher <kegan@compiler.la>
2023-07-06 22:59:21 +0000

    refactor: no need for icon tag, use modified include

--- benefits/core/templatetags/core.py
+++ benefits/core/templatetags/core.py
@@ -3,14 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("core/tags/icon.html")
-def icon(name, alt):
-    """
-    Defines a tag `{% icon name="" alt="" %}`
-    """
-    return {"name": name, "alt": alt}
-
-
 @register.tag
 def media_item(parser, token):
     """
