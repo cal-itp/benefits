9249bb7f76eabc15a1b0fb6bd9c3997532144635
Kegan Maher <kegan@compiler.la>
2023-07-06 04:46:29 +0000

    refactor: template, media item for Login.gov

--- benefits/eligibility/templatetags/eligibility.py
+++ benefits/eligibility/templatetags/eligibility.py
@@ -9,6 +9,11 @@ def media_item__bankcardcheck():
     return {}
 
 
+@register.inclusion_tag("eligibility/tags/media_item__idcardcheck__login_gov.html")
+def media_item__idcardcheck__login_gov():
+    return {}
+
+
 @register.inclusion_tag("eligibility/tags/media_item__idcardcheck__mst_cc.html")
 def media_item__idcardcheck__mst_cc():
     return {}
