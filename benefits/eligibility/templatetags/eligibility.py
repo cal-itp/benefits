a500f0142eccff49117b019976b7961eed71a9b6
Kegan Maher <kegan@compiler.la>
2023-07-06 07:52:33 +0000

    refactor: inline single-use media items
    rename the common start media item

--- benefits/eligibility/templatetags/eligibility.py
+++ benefits/eligibility/templatetags/eligibility.py
@@ -4,21 +4,6 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("eligibility/tags/media_item__bankcardcheck.html")
-def media_item__bankcardcheck():
-    return {}
-
-
-@register.inclusion_tag("eligibility/tags/media_item__idcardcheck__login_gov.html")
-def media_item__idcardcheck__login_gov():
-    return {}
-
-
-@register.inclusion_tag("eligibility/tags/media_item__idcardcheck__mst_cc.html")
-def media_item__idcardcheck__mst_cc():
-    return {}
-
-
-@register.inclusion_tag("eligibility/tags/media_item__idcardcheck__veteran.html")
-def media_item__idcardcheck__veteran():
+@register.inclusion_tag("eligibility/tags/media_item__start__bankcardcheck.html")
+def media_item__start__bankcardcheck():
     return {}
