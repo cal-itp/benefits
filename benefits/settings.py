4f98e63ad7c959454e75ba879a0bcd23402dddaf
Machiko Yasuda <machiko@compiler.la>
2024-02-06 18:26:16 +0000

    fix(settings): remove if ADMIN checks

--- benefits/settings.py
+++ benefits/settings.py
@@ -36,30 +36,29 @@ INSTALLED_APPS = [
     "benefits.oauth",
 ]
 
-if ADMIN:
-    GOOGLE_SSO_CLIENT_ID = os.environ.get("GOOGLE_SSO_CLIENT_ID", "secret")
-    GOOGLE_SSO_PROJECT_ID = os.environ.get("GOOGLE_SSO_PROJECT_ID", "benefits-admin")
-    GOOGLE_SSO_CLIENT_SECRET = os.environ.get("GOOGLE_SSO_CLIENT_SECRET", "secret")
-    GOOGLE_SSO_ALLOWABLE_DOMAINS = _filter_empty(os.environ.get("GOOGLE_SSO_ALLOWABLE_DOMAINS", "compiler.la").split(","))
-    GOOGLE_SSO_STAFF_LIST = _filter_empty(os.environ.get("GOOGLE_SSO_STAFF_LIST", "").split(","))
-    GOOGLE_SSO_SUPERUSER_LIST = _filter_empty(os.environ.get("GOOGLE_SSO_SUPERUSER_LIST", "").split(","))
-    GOOGLE_SSO_LOGO_URL = "/static/img/icon/google_sso_logo.svg"
-    GOOGLE_SSO_SAVE_ACCESS_TOKEN = True
-    GOOGLE_SSO_PRE_LOGIN_CALLBACK = "benefits.admin.pre_login_user"
-    GOOGLE_SSO_SCOPES = [
-        "openid",
-        "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/userinfo.profile",
-    ]
+GOOGLE_SSO_CLIENT_ID = os.environ.get("GOOGLE_SSO_CLIENT_ID", "secret")
+GOOGLE_SSO_PROJECT_ID = os.environ.get("GOOGLE_SSO_PROJECT_ID", "benefits-admin")
+GOOGLE_SSO_CLIENT_SECRET = os.environ.get("GOOGLE_SSO_CLIENT_SECRET", "secret")
+GOOGLE_SSO_ALLOWABLE_DOMAINS = _filter_empty(os.environ.get("GOOGLE_SSO_ALLOWABLE_DOMAINS", "compiler.la").split(","))
+GOOGLE_SSO_STAFF_LIST = _filter_empty(os.environ.get("GOOGLE_SSO_STAFF_LIST", "").split(","))
+GOOGLE_SSO_SUPERUSER_LIST = _filter_empty(os.environ.get("GOOGLE_SSO_SUPERUSER_LIST", "").split(","))
+GOOGLE_SSO_LOGO_URL = "/static/img/icon/google_sso_logo.svg"
+GOOGLE_SSO_SAVE_ACCESS_TOKEN = True
+GOOGLE_SSO_PRE_LOGIN_CALLBACK = "benefits.admin.pre_login_user"
+GOOGLE_SSO_SCOPES = [
+    "openid",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+]
 
-    INSTALLED_APPS.extend(
-        [
-            "django.contrib.admin",
-            "django.contrib.auth",
-            "django.contrib.contenttypes",
-            "django_google_sso",  # Add django_google_sso
-        ]
-    )
+INSTALLED_APPS.extend(
+    [
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django_google_sso",  # Add django_google_sso
+    ]
+)
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -75,13 +74,12 @@ MIDDLEWARE = [
     "benefits.core.middleware.ChangedLanguageEvent",
 ]
 
-if ADMIN:
-    MIDDLEWARE.extend(
-        [
-            "django.contrib.auth.middleware.AuthenticationMiddleware",
-            "django.contrib.messages.middleware.MessageMiddleware",
-        ]
-    )
+MIDDLEWARE.extend(
+    [
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+    ]
+)
 
 if DEBUG:
     MIDDLEWARE.append("benefits.core.middleware.DebugSession")
@@ -144,13 +142,12 @@ if DEBUG:
         ]
     )
 
-if ADMIN:
-    template_ctx_processors.extend(
-        [
-            "django.contrib.auth.context_processors.auth",
-            "django.contrib.messages.context_processors.messages",
-        ]
-    )
+template_ctx_processors.extend(
+    [
+        "django.contrib.auth.context_processors.auth",
+        "django.contrib.messages.context_processors.messages",
+    ]
+)
 
 TEMPLATES = [
     {
@@ -177,23 +174,22 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = []
 
-if ADMIN:
-    AUTH_PASSWORD_VALIDATORS.extend(
-        [
-            {
-                "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-            },
-            {
-                "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-            },
-            {
-                "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-            },
-            {
-                "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-            },
-        ]
-    )
+AUTH_PASSWORD_VALIDATORS.extend(
+    [
+        {
+            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        },
+        {
+            "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        },
+        {
+            "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        },
+        {
+            "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        },
+    ]
+)
 
 # Internationalization
 
