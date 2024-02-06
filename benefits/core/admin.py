a0c4b2ea5223cd9689a78d3d8c15deb4e010a9db
Machiko Yasuda <machiko@compiler.la>
2024-02-06 18:19:34 +0000

    refactor(admin): move code to Admin.py

--- benefits/core/admin.py
+++ benefits/core/admin.py
@@ -2,8 +2,9 @@
 The core application: Admin interface configuration.
 """
 
-from django.conf import settings
+import requests
 
+from django.conf import settings
 
 if settings.ADMIN:
     import logging
@@ -21,3 +22,23 @@ if settings.ADMIN:
     ]:
         logger.debug(f"Register {model.__name__}")
         admin.site.register(model)
+
+    def pre_login_user(user, request):
+        logger.debug(f"Running pre-login callback for user: {user.username}")
+        token = request.session.get("google_sso_access_token")
+        if token:
+            headers = {
+                "Authorization": f"Bearer {token}",
+            }
+
+            # Request Google user info to get name and email
+            url = "https://www.googleapis.com/oauth2/v3/userinfo"
+            response = requests.get(url, headers=headers, timeout=settings.REQUESTS_TIMEOUT)
+            user_data = response.json()
+            logger.debug(f"Updating admin user data from Google for user with email: {user_data['email']}")
+
+            user.first_name = user_data["given_name"]
+            user.last_name = user_data["family_name"]
+            user.username = user_data["email"]
+            user.email = user_data["email"]
+            user.save()
