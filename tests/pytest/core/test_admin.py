84956df1d0b7ef6c73fe972e54ecbd3ef51a6b21
Kegan Maher <kegan@compiler.la>
2024-02-13 17:38:56 +0000

    test(admin): assert redirects to login page

--- tests/pytest/core/test_admin.py
+++ tests/pytest/core/test_admin.py
@@ -3,6 +3,10 @@ import pytest
 
 @pytest.mark.django_db
 def test_admin_registered(client):
-    response = client.get("/admin")
+    response = client.get("/admin", follow=True)
 
-    assert response.status_code == 301
+    assert response.status_code == 200
+    assert ("/admin/", 301) in response.redirect_chain
+    assert ("/admin/login/?next=/admin/", 302) in response.redirect_chain
+    assert response.request["PATH_INFO"] == "/admin/login/"
+    assert "google_sso/login.html" in response.template_name
