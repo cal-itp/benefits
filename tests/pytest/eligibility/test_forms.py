a2c15d8bc2c030586e833491d15a885f3f60572e
Angela Tran <angela@compiler.la>
2024-04-23 19:05:36 +0000

    test: add assertions for form widget attributes

--- tests/pytest/eligibility/test_forms.py
+++ tests/pytest/eligibility/test_forms.py
@@ -6,8 +6,28 @@ def test_MSTCourtesyCard():
 
     assert form.is_valid()
 
+    sub_attrs = form.fields["sub"].widget.attrs
+    assert sub_attrs["pattern"] == r"\d{5}"
+    assert sub_attrs["inputmode"] == "numeric"
+    assert sub_attrs["maxlength"] == 5
+    assert sub_attrs["data-custom-validity"] == "Please enter a 5-digit number."
+
+    name_attrs = form.fields["name"].widget.attrs
+    assert name_attrs["maxlength"] == 255
+    assert name_attrs["data-custom-validity"] == "Please enter your last name."
+
 
 def test_SBMTDMobilityPass():
     form = SBMTDMobilityPass(data={"sub": "1234", "name": "Barbara"})
 
     assert form.is_valid()
+
+    sub_attrs = form.fields["sub"].widget.attrs
+    assert sub_attrs["pattern"] == r"\d{4}"
+    assert sub_attrs["maxlength"] == 4
+    assert sub_attrs["inputmode"] == "numeric"
+    assert sub_attrs["data-custom-validity"] == "Please enter a 4-digit number."
+
+    name_attrs = form.fields["name"].widget.attrs
+    assert name_attrs["maxlength"] == 255
+    assert name_attrs["data-custom-validity"] == "Please enter your last name."
