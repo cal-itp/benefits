1d4eb88a91d1052d5eaf6477ec34d1134e1397d0
Angela Tran <angela@compiler.la>
2024-05-30 20:48:44 +0000

    fix(test): remove test that has been revealed to not be meaningful
    when the fixture was a Mock object, the test saw `uses_auth_verification`
    as a function, but now that the fixture is using an actual model object,
    the `@property` decorator obscures the function and makes it look like
    a `bool` type.
    
    the intent of the test was to ensure the function is called, but that is
    already covered by the `bool` being `True` which is already a part of
    other tests.

--- tests/pytest/oauth/test_analytics.py
+++ tests/pytest/oauth/test_analytics.py
@@ -4,15 +4,6 @@ from benefits.oauth.analytics import OAuthEvent
 
 
 @pytest.mark.django_db
-def test_OAuthEvent_checks_verifier_uses_auth_verification(app_request, mocked_session_verifier_uses_auth_verification):
-    mocked_verifier = mocked_session_verifier_uses_auth_verification(app_request)
-
-    OAuthEvent(app_request, "event type")
-
-    mocked_verifier.uses_auth_verification.assert_called_once
-
-
-@pytest.mark.django_db
 def test_OAuthEvent_verifier_client_name_when_uses_auth_verification(
     app_request, mocked_session_verifier_uses_auth_verification
 ):
