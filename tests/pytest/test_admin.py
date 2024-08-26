b5d111461e675c2221e58581f3780593825db27f
Luis Alvergue <luis@compiler.la>
2024-08-26 21:38:53 +0000

    chore: fix naming and how to set booleans in User models

--- tests/pytest/test_admin.py
+++ tests/pytest/test_admin.py
@@ -6,7 +6,7 @@ from benefits.admin import BenefitsAdminSite
 
 @pytest.fixture
 def model_User():
-    return User.objects.create(is_active="1", is_staff="1")
+    return User.objects.create(is_active=True, is_staff=True)
 
 
 @pytest.fixture
@@ -18,7 +18,7 @@ def model_AgencyUser(model_User):
 
 
 @pytest.fixture
-def model_SuperAgencyUser(model_User):
+def model_StaffGroupUser(model_User):
     model_User.groups.add(Group.objects.get(name=settings.STAFF_GROUP_NAME))
     model_User.save()
     return model_User
@@ -26,7 +26,7 @@ def model_SuperAgencyUser(model_User):
 
 @pytest.fixture
 def model_SuperUser(model_User):
-    model_User.is_superuser = "1"
+    model_User.is_superuser = True
     model_User.save()
     return model_User
 
@@ -41,8 +41,8 @@ def test_admin_override_agency_user(app_request, model_AgencyUser):
 
 
 @pytest.mark.django_db
-def test_admin_override_super_agency_user(app_request, model_SuperAgencyUser):
-    app_request.user = model_SuperAgencyUser
+def test_admin_override_staff_group_user(app_request, model_StaffGroupUser):
+    app_request.user = model_StaffGroupUser
     response = BenefitsAdminSite().index(app_request)
 
     assert response.status_code == 200
