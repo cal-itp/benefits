87cbb1e3a4b399ab885b65970f3ad8d74cad4259
Luis Alvergue <luis@compiler.la>
2024-09-26 14:00:44 +0000

    feat(analytics): add started and ended card tokenization for in-person

--- benefits/in_person/views.py
+++ benefits/in_person/views.py
@@ -132,6 +132,7 @@ def enrollment(request):
             "card_tokenize_env": agency.transit_processor.card_tokenize_env,
             "card_tokenize_func": agency.transit_processor.card_tokenize_func,
             "card_tokenize_url": agency.transit_processor.card_tokenize_url,
+            "enrollment_method": models.EnrollmentMethods.IN_PERSON,
             "token_field": "card_token",
             "form_retry": tokenize_retry_form.id,
             "form_server_error": tokenize_server_error_form.id,
