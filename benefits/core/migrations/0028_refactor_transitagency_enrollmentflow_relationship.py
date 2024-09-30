b07757aba6847f21ed58702e93e5d0571c0cb642
Machiko Yasuda <machiko@compiler.la>
2024-09-30 06:28:17 +0000

    feat(db): allow enrollmentflow.transit_agency to be null

--- benefits/core/migrations/0028_refactor_transitagency_enrollmentflow_relationship.py
+++ benefits/core/migrations/0028_refactor_transitagency_enrollmentflow_relationship.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="enrollmentflow",
             name="transit_agency",
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.PROTECT, to="core.transitagency"),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to="core.transitagency"),
             preserve_default=False,
         ),
         migrations.RunPython(migrate_data),
