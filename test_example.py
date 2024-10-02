2ee66e5ca24964d5e396080694ee0254e2dc43e7
Angela Tran <angela@compiler.la>
2024-10-02 15:34:49 +0000

    chore: proof of concept changes to install playwright

---
+++ test_example.py
@@ -0,0 +1,21 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_has_title(page: Page):
+    page.goto("https://dev-benefits.calitp.org/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Cal-ITP"))
+
+
+def test_get_started_link(page: Page):
+    page.goto("https://dev-benefits.calitp.org/")
+
+    # Click the Choose your provider link.
+    page.get_by_text("Choose your provider").click()
+
+    page.get_by_text("California State Transit (dev)").click()
+
+    # # Expects page to have a heading with the name of Installation.
+    # expect(page.get("heading", name="Installation")).to_be_visible()
