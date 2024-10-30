8e2ddb1a2b1af1575d2e707cf0ab6cd65061e957
Kegan Maher <kegan@compiler.la>
2024-10-30 21:46:22 +0000

    feat(cli): structure for any command to have subcommands

--- benefits/cli/commands.py
+++ benefits/cli/commands.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import Callable
+
 from django.core.management.base import BaseCommand
 
 from benefits import VERSION
@@ -6,6 +8,7 @@ from benefits import VERSION
 
 @dataclass
 class BaseOptions:
+    handler: Callable = None
     skip_checks: bool = False
     verbosity: int = 1
 
@@ -16,14 +19,90 @@ class BenefitsCommand(BaseCommand):
     name = "benefits"
     help = __doc__
     options_cls = BaseOptions
+    subcommands = []
     version = VERSION
 
-    def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False):
+    def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False, default_subcmd=None):
         super().__init__(stdout, stderr, no_color, force_color)
+
+        self.commands = set()
+        # for each subcommand declared on the BenefitsCommand class (or subclass)
+        # instantiate it and add it to the commands set
+        self.commands.update(cmd(stdout, stderr, no_color, force_color) for cmd in self.subcommands)
+        # set an attribute on this instance for each command
+        for cmd in self.commands:
+            setattr(self, cmd.name, cmd)
+        # can be used by subclasses to track their own argument subparsers
+        self.subparsers = {self.name: {}}
+
+        # default_subcmd should be one of the items in BenefitsCommand.subcommands (or subclass.subcommands)
+        # i.e. a class that implements one of the subcommands for this BenefitsCommand (or subclass)
+        if default_subcmd:
+            if default_subcmd in self.subcommands:
+                # we want to store the handle method for the instance of this default_subcmd class
+                # read command attribute created above
+                # the default handler is a function to call when the command is called without a subcommand
+                self.default_handler = getattr(self, default_subcmd.name).handle
+            else:
+                raise ValueError("default_subcmd must be in this Command's declared subcommands list.")
+        else:
+            self.default_handler = None
+
+    @property
+    def subparser(self):
+        """Gets the single self.subparsers with a name matching this command's."""
+        return self.subparsers[self.name]
+
+    def add_arguments(self, parser):
+        """Entry point for subclassed commands to add custom arguments."""
+        if len(self.commands) < 1:
+            return
+
+        # For each command this BenefitsCommand instance defines
+        # create an argparse subparser for that command's arguments
+        # adapted from https://adamj.eu/tech/2024/08/14/django-management-command-sub-commands/
+        command_required = self.default_handler is None
+        subparsers = parser.add_subparsers(title="commands", required=command_required)
+
+        for command in self.commands:
+            subparser = subparsers.add_parser(command.name, help=command.help)
+            # command is an instance inheriting from BenefitsCommand
+            # so it has an .add_arguments() method
+            # add them to the command's subparser
+            command.add_arguments(subparser)
+            # set_defaults makes the resulting "handler" argument equal to the
+            # command's handle function
+            subparser.set_defaults(handler=command.handle)
+            # store a reference to each command subparser
+            self.subparser[command.name] = subparser
 
     def get_version(self) -> str:
         """Override `BaseCommand.get_version()` to return the `benefits` version."""
         return self.version
+
+    def handle(self, *args, **options):
+        """The actual logic of the command. Subclasses of Django's `BaseCommand` must implement this method.
+
+        The default implementation for `BenefitsCommands` implements a pattern like:
+
+        ```
+        command --cmd-opt subcommand --subcmd-opt subcmd_arg
+        ```
+
+        Subclasses of `BenefitsCommand` may provide a custom implementation as-needed.
+        """
+        # by default, parse options as a BaseOptions instance
+        opts = self.parse_opts(**options)
+        # get the handler parsed from the options, or the default_handler
+        handler = opts.handler or self.default_handler
+
+        if handler:
+            if opts.verbosity > 1:
+                # handler is a class method, use its __self__ prop to get back to the instance
+                command_name = handler.__self__.name
+                self.stdout.write(self.style.WARNING(f"command: {command_name}"))
+            # call it
+            handler(*args, **options)
 
     def parse_opts(self, **options):
         """Parse options into a dataclass instance."""
--- tests/pytest/cli/test_commands.py
+++ tests/pytest/cli/test_commands.py
@@ -1,30 +1,111 @@
+from dataclasses import dataclass
+
 import pytest
 
 from benefits import VERSION
 from benefits.cli.commands import BaseOptions, BenefitsCommand
 
 
+@dataclass
+class MockOptions(BaseOptions):
+    str_option: str = ""
+    int_option: int = 0
+
+
+class MockCommand(BenefitsCommand):
+    """This is a mock command."""
+
+    name = "mock"
+    # needs to be [] for this test to avoid infinite recursion!
+    subcommands = []
+
+
 @pytest.fixture
-def benefits_cmd():
-    return BenefitsCommand()
+def with_subcommand():
+    # fake a class definition with subcommands
+    # using the above MockCommand
+    BenefitsCommand.subcommands.append(MockCommand)
+    # yield to allow the test to continue
+    yield
+    # cleanup
+    BenefitsCommand.subcommands.clear()
+
+
+@pytest.fixture
+def with_custom_opts():
+    # fake the options_cls
+    # using the above MockOptions
+    BenefitsCommand.options_cls = MockOptions
+    # yield to allow the test to continue
+    yield
+    # cleanup
+    BenefitsCommand.options_cls = BaseOptions
 
 
 @pytest.mark.django_db
-def test_BenefitsCommand_init(benefits_cmd):
-    assert benefits_cmd.name == "benefits"
-    assert benefits_cmd.help == BenefitsCommand.__doc__
-    assert benefits_cmd.options_cls == BaseOptions
-    assert benefits_cmd.version == VERSION
+def test_class():
+    assert BenefitsCommand.help == BenefitsCommand.__doc__
+    assert BenefitsCommand.name == "benefits"
+    assert BenefitsCommand.options_cls is BaseOptions
+    assert BenefitsCommand.subcommands == []
+    assert BenefitsCommand.version == VERSION
 
 
 @pytest.mark.django_db
-def test_BenefitsCommand_get_version(benefits_cmd):
-    assert benefits_cmd.get_version() == VERSION
+@pytest.mark.usefixtures("with_subcommand")
+def test_subcommands():
+    cmd = BenefitsCommand(default_subcmd=MockCommand)
+
+    # it should have parsed cls.subcommands into self.commands
+    assert len(cmd.commands) == 1
+
+    # get the subcommand
+    subcmd = list(cmd.commands).pop()
+    # it should be a MockCommand
+    assert isinstance(subcmd, MockCommand)
+
+    # the instance
+    assert hasattr(cmd, MockCommand.name)
+    assert getattr(cmd, MockCommand.name) == subcmd
+    # should have a default_handler
+    assert cmd.default_handler == subcmd.handle
 
 
 @pytest.mark.django_db
-def test_BenefitsCommand_parse_opts(benefits_cmd):
-    opts = benefits_cmd.parse_opts(skip_checks=True, verbosity=3)
+def test_init():
+    cmd = BenefitsCommand()
 
+    assert cmd.commands == set()
+    assert cmd.subparsers == {"benefits": {}}
+
+
+@pytest.mark.django_db
+def test_get_version():
+    assert BenefitsCommand().get_version() == VERSION
+
+
+@pytest.mark.django_db
+def test_parse_opts():
+    handler = lambda x: x  # noqa: E731
+    cmd = BenefitsCommand()
+    opts = cmd.parse_opts(handler=handler, skip_checks=True, verbosity=3)
+
+    assert opts.handler == handler
     assert opts.skip_checks
     assert opts.verbosity == 3
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("with_custom_opts")
+def test_parse_custom_opts():
+    cmd = BenefitsCommand()
+
+    opts = cmd.parse_opts()
+    assert isinstance(opts, MockOptions)
+    assert opts.str_option == ""
+    assert opts.int_option == 0
+
+    opts = cmd.parse_opts(str_option="str", int_option=100)
+    assert isinstance(opts, MockOptions)
+    assert opts.str_option == "str"
+    assert opts.int_option == 100
