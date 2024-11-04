fed78b6acc2416d2f81f5a7f2f7d8e9c8bfd3f2c
Angela Tran <angela@compiler.la>
2024-11-04 06:06:13 +0000

    refactor: extract some helper script args out into pytest.ini file
    this makes it so they are used any time pytest is invoked from this
    directory

--- playwright/run.sh
+++ playwright/run.sh
@@ -4,4 +4,5 @@ set -e
 
 base_url="https://dev-benefits.calitp.org"
 
-pytest --base-url $base_url --tracing on -v --template=html1/index.html --report=test-results/report.html --video on
+# also uses arguments from pytest.ini
+pytest --base-url $base_url
