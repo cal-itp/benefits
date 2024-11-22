5d3c957317a06949871c9de056a37905d5dcb91b
Angela Tran <angela@compiler.la>
2024-11-22 20:03:13 +0000

    chore: remove GitHub workflow for running Cypress tests

--- bin/test_start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-# container startup script specifically for running Cypress tests
-# needs to reset the DB with sample data and then start the app normally
-
-bin/reset_db.sh
-
-bin/start.sh
