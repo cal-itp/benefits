d4f4207880023d9ce447409f7da386a64f5e949d
Angela Tran <angela@compiler.la>
2024-12-04 01:23:19 +0000

    chore: proof of concept for using custom markers to organize tests
    env_base_url calculates the base URL based on marker name.
    
    we can run tests for a specific environment by passing in the marker
    name, e.g. pytest -m dev

--- tests/playwright/test_healthcheck.py
+++ tests/playwright/test_healthcheck.py
@@ -1,7 +1,37 @@
 from playwright.sync_api import Page, expect
+import pytest
 
 
-def test_dev_healthcheck(page: Page):
-    page.goto("https://dev-benefits.calitp.org/healthcheck")
+@pytest.fixture
+def env_base_url(request):
+    marker = (
+        request.node.get_closest_marker("dev")
+        or request.node.get_closest_marker("test")
+        or request.node.get_closest_marker("prod")
+    )
+    print(marker)
+
+    if marker:
+        env = marker.name
+    else:
+        env = None
+
+    if env == "dev":
+        return "https://dev-benefits.calitp.org"
+    elif env == "test":
+        return "https://test-benefits.calitp.org"
+    elif env == "prod":
+        return "http://dev-benefits.calitp.org"  # use dev for now, don't want to spam prod
+    else:
+        return "http://localhost:11369"  # use port that will work with IdG
+
+
+def env(*marks):
+    return pytest.mark.parametrize("env_name", [pytest.param(mark.name, marks=mark) for mark in marks])
+
+
+@env(pytest.mark.dev, pytest.mark.test, pytest.mark.prod)
+def test_healthcheck(page: Page, env_base_url, env_name):
+    page.goto(env_base_url + "/healthcheck")
 
     expect(page.get_by_text("Healthy")).to_be_visible()
