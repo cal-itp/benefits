2f6dd51509f03d98d99315e59d5a62e8305b1b4e
Angela Tran <angela@compiler.la>
2024-12-04 01:20:48 +0000

    chore: proof of concept where both marker and base-url are needed

--- tests/playwright/run.sh
+++ tests/playwright/run.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-set -e
+set -eux
 
-pytest
+ENV=$1
+
+if [ $# -ne 1 ]; then
+  echo "No environment given... running against local environment"
+else
+  MARK="-m ${ENV}"
+fi
+
+if [ "$ENV" = "dev" ]; then
+  BASE_URL="https://dev-benefits.calitp.org"
+elif [ "$ENV" = "test" ]; then
+  BASE_URL="https://test-benefits.calitp.org"
+elif [ "$ENV" = "prod" ]; then
+  BASE_URL="https://benefits.calitp.org"
+else
+  BASE_URL="http://localhost:11369"
+  MARK=""
+fi
+
+pytest --base-url "$BASE_URL" "$MARK"
--- tests/playwright/test_healthcheck.py
+++ tests/playwright/test_healthcheck.py
@@ -1,7 +1,11 @@
 from playwright.sync_api import Page, expect
+import pytest
 
 
-def test_dev_healthcheck(page: Page):
-    page.goto("https://dev-benefits.calitp.org/healthcheck")
+@pytest.mark.dev
+@pytest.mark.test
+@pytest.mark.prod
+def test_healthcheck(page: Page):
+    page.goto("/healthcheck")
 
     expect(page.get_by_text("Healthy")).to_be_visible()
