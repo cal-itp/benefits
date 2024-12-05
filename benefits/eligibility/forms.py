a10e1acd21a2c9d6ca810db3571254b296800474
Machiko Yasuda <machiko@compiler.la>
2024-12-05 19:53:06 +0000

    feat(recaptcha): convert elig page to all col-lg-6

--- benefits/eligibility/forms.py
+++ benefits/eligibility/forms.py
@@ -28,7 +28,7 @@ class EnrollmentFlowSelectionForm(forms.Form):
         super().__init__(*args, **kwargs)
         flows = agency.enrollment_flows.filter(supported_enrollment_methods__contains=models.EnrollmentMethods.DIGITAL)
 
-        self.classes = "col-lg-8"
+        self.classes = "col-lg-6"
         # second element is not used since we render the whole label using selection_label_template,
         # therefore set to None
         flow_field = self.fields["flow"]
