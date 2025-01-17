3b315b26d5c672323cdefe4e912525e0c5eb8eb4
Kegan Maher <kegan@compiler.la>
2025-01-17 18:37:59 +0000

    feat(admin): field/add permissions for Common models

--- benefits/core/admin/common.py
+++ benefits/core/admin/common.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+from django.http import HttpRequest
 
 from benefits.core import models
 
 
 @admin.register(models.PemData)
 class PemDataAdmin(admin.ModelAdmin):
-    list_display = ("label",)
+
+    def has_view_permission(self, request: HttpRequest, obj=None):
+        if request.user and request.user.is_superuser:
+            return True
+        else:
+            return False
+
+    def has_add_permission(self, request: HttpRequest, obj=None):
+        if request.user and request.user.is_superuser:
+            return True
+        else:
+            return False
