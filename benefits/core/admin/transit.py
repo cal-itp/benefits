fd36b2f3bc4c54eccd078876b4fa928236c51416
Kegan Maher <kegan@compiler.la>
2025-01-17 20:35:50 +0000

    feat(admin): field/add permissions for Transit models

--- benefits/core/admin/transit.py
+++ benefits/core/admin/transit.py
@@ -1,48 +1,74 @@
+from django.conf import settings
 from django.contrib import admin
 
 from benefits.core import models
+from .users import is_staff_member_or_superuser
 
 
 @admin.register(models.TransitAgency)
-class TransitAgencyAdmin(admin.ModelAdmin):  # pragma: no cover
+class TransitAgencyAdmin(admin.ModelAdmin):
     def get_exclude(self, request, obj=None):
+        fields = []
+
         if not request.user.is_superuser:
-            return [
-                "eligibility_api_private_key",
-                "eligibility_api_public_key",
-                "transit_processor_client_id",
-                "transit_processor_client_secret_name",
-                "transit_processor_audience",
-            ]
-        else:
-            return super().get_exclude(request, obj)
+            fields.extend(
+                [
+                    "eligibility_api_private_key",
+                    "eligibility_api_public_key",
+                    "sso_domain",
+                    "transit_processor_audience",
+                    "transit_processor_client_id",
+                    "transit_processor_client_secret_name",
+                ]
+            )
+
+        return fields or super().get_exclude(request, obj)
 
     def get_readonly_fields(self, request, obj=None):
+        fields = []
+
         if not request.user.is_superuser:
-            return [
-                "eligibility_api_id",
-                "transit_processor",
-                "index_template_override",
-                "eligibility_index_template_override",
-            ]
+            fields.extend(
+                [
+                    "index_template_override",
+                    "eligibility_index_template_override",
+                    "eligibility_api_id",
+                    "transit_processor",
+                ]
+            )
+
+        return fields or super().get_readonly_fields(request, obj)
+
+    def has_add_permission(self, request):
+        if settings.RUNTIME_ENVIRONMENT() != settings.RUNTIME_ENVS.PROD:
+            return True
+        elif request.user and is_staff_member_or_superuser(request.user):
+            return True
         else:
-            return super().get_readonly_fields(request, obj)
+            return False
 
 
 @admin.register(models.TransitProcessor)
-class TransitProcessorAdmin(admin.ModelAdmin):  # pragma: no cover
-    def get_exclude(self, request, obj=None):
-        if not request.user.is_superuser:
-            return []
-        else:
-            return super().get_exclude(request, obj)
+class TransitProcessorAdmin(admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):
+        fields = []
+
         if not request.user.is_superuser:
-            return [
-                "card_tokenize_url",
-                "card_tokenize_func",
-                "card_tokenize_env",
-            ]
+            fields.extend(
+                [
+                    "card_tokenize_url",
+                    "card_tokenize_func",
+                    "card_tokenize_env",
+                ]
+            )
+
+        return fields or super().get_readonly_fields(request, obj)
+
+    def has_add_permission(self, request):
+        if settings.RUNTIME_ENVIRONMENT() != settings.RUNTIME_ENVS.PROD:
+            return True
+        elif request.user and request.user.is_superuser:
+            return True
         else:
-            return super().get_readonly_fields(request, obj)
+            return False
