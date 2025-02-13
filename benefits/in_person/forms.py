d9bec5104dda5f14bdd26ce5d9a47131738c61c5
Angela Tran <angela@compiler.la>
2025-02-13 21:22:10 +0000

    feat: implement back-end form validation for in-person eligibility
    previously we could rely on Django's default handling of the single
    `BooleanField`, but since we have multiple checkboxes now, and we don't
    want to require all of them, we need to implement our own logic to
    require that the checkbox that matches the selected flow was checked.
    
    also added Javascript to the template to show the checkbox for the case
    where the form didn't pass back-end validation and therefore we show
    the form again with what the user had selected.

--- benefits/in_person/forms.py
+++ benefits/in_person/forms.py
@@ -21,6 +21,9 @@ class InPersonEligibilityForm(forms.Form):
 
     cancel_url = routes.ADMIN_INDEX
 
+    flow_field_error_message = "Choose an eligibility type"
+    verified_field_error_message = "Check the box to verify you have confirmed eligibility"
+
     def __init__(self, agency: models.TransitAgency, *args, **kwargs):
         super().__init__(*args, **kwargs)
         flows = agency.enrollment_flows.filter(supported_enrollment_methods__contains=models.EnrollmentMethods.IN_PERSON)
@@ -28,17 +31,19 @@ class InPersonEligibilityForm(forms.Form):
         self.classes = "in-person-eligibility-form"
         flow_field = self.fields["flow"]
         flow_field.choices = [(f.id, f.label) for f in flows]
-        flow_field.widget.attrs.update({"data-custom-validity": "Choose an eligibility type"})
+        flow_field.widget.attrs.update({"data-custom-validity": self.flow_field_error_message})
 
         # dynamically add a BooleanField for each flow
         for flow in flows:
             field_id = f"verified_{flow.id}"
             self.fields[field_id] = forms.BooleanField(
-                label=self.get_policy_details(flow), widget=forms.widgets.CheckboxInput(attrs={"class": "d-none"})
+                required=False,  # `clean()` will handle requiring the specific field
+                label=self.get_policy_details(flow),
+                widget=forms.widgets.CheckboxInput(attrs={"class": "d-none"}),  # start out hidden
             )
             field = self.fields[field_id]
             field.hide = True
-            field.widget.attrs.update({"data-custom-validity": "Check the box to verify you have confirmed eligibility"})
+            field.widget.attrs.update({"data-custom-validity": self.verified_field_error_message})
 
         self.use_custom_validity = True
 
@@ -46,3 +51,17 @@ class InPersonEligibilityForm(forms.Form):
         eligibility_context = flow.in_person_eligibility_context
 
         return eligibility_context["policy_details"] if eligibility_context else None
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        flow_field_name = "flow"
+        selected_flow = cleaned_data.get(flow_field_name)
+
+        if not selected_flow:
+            self.add_error(flow_field_name, self.flow_field_error_message)
+        else:
+            verified_field_name = f"verified_{selected_flow}"
+            verified = cleaned_data.get(verified_field_name)
+            if not verified:
+                self.add_error(verified_field_name, self.verified_field_error_message)
