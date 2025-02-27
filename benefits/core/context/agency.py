3d1cb983755fe8797b78826dcd3e05c4abe08a58
Angela Tran <angela@compiler.la>
2025-02-27 04:37:36 +0000

    fix(i18n): put back escaped percent sign in string, compute value
    this makes it so the template gets the computed value, and
    `makemessages` and `compilemessages` get the literal value.

--- benefits/core/context/agency.py
+++ benefits/core/context/agency.py
@@ -46,32 +46,41 @@ class EligibilityIndex:
 eligibility_index = {
     AgencySlug.CST.value: EligibilityIndex(
         form_text=_(
-            "Cal-ITP doesn’t save any of your information. All CST transit benefits reduce fares by 50% for bus service on fixed routes."  # noqa
+            "Cal-ITP doesn’t save any of your information. "
+            "All CST transit benefits reduce fares by 50%% for bus service on fixed routes.".replace("%%", "%")
         )
     ),
     AgencySlug.MST.value: EligibilityIndex(
         form_text=_(
-            "Cal-ITP doesn’t save any of your information. All MST transit benefits reduce fares by 50% for bus service on fixed routes."  # noqa
+            "Cal-ITP doesn’t save any of your information. "
+            "All MST transit benefits reduce fares by 50%% for bus service on fixed routes.".replace("%%", "%")
         )
     ),
     AgencySlug.NEVCO.value: EligibilityIndex(
         form_text=_(
-            "Cal-ITP doesn’t save any of your information. All Nevada County Connects transit benefits reduce fares by 50% for bus service on fixed routes."  # noqa
+            "Cal-ITP doesn’t save any of your information. "
+            "All Nevada County Connects transit benefits reduce fares by 50%% for bus service on fixed routes.".replace(
+                "%%", "%"
+            )
         )
     ),
     AgencySlug.SACRT.value: EligibilityIndex(
         form_text=[
             _(
-                "Cal-ITP doesn’t save any of your information. All SacRT transit benefits reduce fares by 50% for bus service on fixed routes."  # noqa
+                "Cal-ITP doesn’t save any of your information. "
+                "All SacRT transit benefits reduce fares by 50%% for bus service on fixed routes.".replace("%%", "%")  # noqa
             ),
             _(
-                "With the new Tap2Ride fare system launching in 2025, SacRT riders that are eligible for discount fares can enroll below. The discount benefit will be available on all SacRT buses beginning in early 2025 and will include light rail tap devices later in the year."  # noqa
+                "With the new Tap2Ride fare system launching in 2025, SacRT riders that are eligible for discount fares can "
+                "enroll below. The discount benefit will be available on all SacRT buses beginning in early 2025 and will "
+                "include light rail tap devices later in the year."  # noqa
             ),
         ]
     ),
     AgencySlug.SBMTD.value: EligibilityIndex(
         form_text=_(
-            "Cal-ITP doesn’t save any of your information. All SBMTD transit benefits reduce fares by 50% for bus service on fixed routes."  # noqa
+            "Cal-ITP doesn’t save any of your information. "
+            "All SBMTD transit benefits reduce fares by 50%% for bus service on fixed routes.".replace("%%", "%")
         )
     ),
 }
