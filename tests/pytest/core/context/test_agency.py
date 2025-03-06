a642d184ea9330668e170b19d660177819a1847d
Angela Tran <angela@compiler.la>
2025-03-06 21:09:30 +0000

    test: check that every AgencySlug has a context in `eligibility_index`

--- tests/pytest/core/context/test_agency.py
+++ tests/pytest/core/context/test_agency.py
@@ -6,3 +6,8 @@ from benefits.core import context as core_context
 @pytest.mark.parametrize("slug", core_context.AgencySlug)
 def test_agency_index(slug):
     assert core_context.agency_index[slug.value]
+
+
+@pytest.mark.parametrize("slug", core_context.AgencySlug)
+def test_eligibility_index(slug):
+    assert core_context.eligibility_index[slug.value]
