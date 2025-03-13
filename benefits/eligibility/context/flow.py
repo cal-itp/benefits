6e067f9d3a6a6c41dbb6ed11ab7c405efbdb5fec
Angela Tran <angela@compiler.la>
2025-03-13 18:12:59 +0000

    refactor(context): extract common values for Agency Cards to de-dupe

--- benefits/eligibility/context/flow.py
+++ benefits/eligibility/context/flow.py
@@ -32,21 +32,27 @@ class LoginGovEligibilityStart(EligibilityStart):
         )
 
 
+class AgencyCardEligibilityStart(EligibilityStart):
+    def __init__(self, headline_text, eligibility_item_template):
+        super().__init__(
+            page_title=_("Agency card overview"),
+            headline_text=headline_text,
+            eligibility_item_template=eligibility_item_template,
+            call_to_action_button=CTAButton(text=_("Continue"), route=routes.ELIGIBILITY_CONFIRM),
+        )
+
+
 eligibility_start = {
-    SystemName.AGENCY_CARD.value: EligibilityStart(
-        page_title=_("Agency card overview"),
+    SystemName.AGENCY_CARD.value: AgencyCardEligibilityStart(
         headline_text=_("You selected an Agency Card transit benefit."),
         eligibility_item_template="eligibility/includes/eligibility-item--identification--start--cst-agency-card.html",
-        call_to_action_button=CTAButton(text=_("Continue"), route=routes.ELIGIBILITY_CONFIRM),
     ),
     SystemName.CALFRESH.value: LoginGovEligibilityStart(
         page_title=_("CalFresh benefit overview"), headline_text=_("You selected a CalFresh Cardholder transit benefit.")
     ),
-    SystemName.COURTESY_CARD.value: EligibilityStart(
-        page_title=_("Agency card overview"),
+    SystemName.COURTESY_CARD.value: AgencyCardEligibilityStart(
         headline_text=_("You selected a Courtesy Card transit benefit."),
         eligibility_item_template="eligibility/includes/eligibility-item--identification--start--mst-agency-card.html",
-        call_to_action_button=CTAButton(text=_("Continue"), route=routes.ELIGIBILITY_CONFIRM),
     ),
     SystemName.MEDICARE.value: EligibilityStart(
         page_title=_("Medicare benefit overview"),
@@ -58,11 +64,9 @@ eligibility_start = {
         page_title=_("Older Adult benefit overview"),
         headline_text=_("You selected an Older Adult transit benefit."),
     ),
-    SystemName.REDUCED_FARE_MOBILITY_ID.value: EligibilityStart(
-        page_title=_("Agency card overview"),
+    SystemName.REDUCED_FARE_MOBILITY_ID.value: AgencyCardEligibilityStart(
         headline_text=_("You selected a Reduced Fare Mobility ID transit benefit."),
         eligibility_item_template="eligibility/includes/eligibility-item--identification--start--sbmtd-agency-card.html",
-        call_to_action_button=CTAButton(text=_("Continue"), route=routes.ELIGIBILITY_CONFIRM),
     ),
     SystemName.VETERAN.value: LoginGovEligibilityStart(
         page_title=_("Veterans benefit overview"), headline_text=_("You selected a Veteran transit benefit.")
