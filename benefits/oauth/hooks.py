adccf681c36deff04d01b5b9a26659b94fbb96fe
Angela Tran <angela@compiler.la>
2025-03-17 21:24:42 +0000

    feat(login): implement `OAuthHooks.system_error`

--- benefits/oauth/hooks.py
+++ benefits/oauth/hooks.py
@@ -1,5 +1,8 @@
 from cdt_identity.hooks import DefaultHooks
+from django.shortcuts import redirect
+import sentry_sdk
 
+from benefits.routes import routes
 from . import analytics
 
 
@@ -8,3 +11,10 @@ class OAuthHooks(DefaultHooks):
     def pre_login(cls, request):
         super().pre_login(request)
         analytics.started_sign_in(request)
+
+    @classmethod
+    def system_error(cls, request, exception):
+        super().system_error(request, exception)
+        analytics.error(request, message=str(exception), operation="authorize_redirect")
+        sentry_sdk.capture_exception(exception)
+        return redirect(routes.OAUTH_SYSTEM_ERROR)
--- tests/pytest/oauth/test_hooks.py
+++ tests/pytest/oauth/test_hooks.py
@@ -1,5 +1,8 @@
+from django.urls import reverse
+
 import pytest
 
+from benefits.routes import routes
 from benefits.oauth.hooks import OAuthHooks
 import benefits.oauth.hooks
 
@@ -9,7 +12,21 @@ def mocked_analytics_module(mocked_analytics_module):
     return mocked_analytics_module(benefits.oauth.hooks)
 
 
+@pytest.fixture
+def mocked_sentry_sdk_module(mocker):
+    return mocker.patch.object(benefits.oauth.hooks, "sentry_sdk")
+
+
 def test_pre_login(app_request, mocked_analytics_module):
     OAuthHooks.pre_login(app_request)
 
     mocked_analytics_module.started_sign_in.assert_called_once()
+
+
+def test_system_error(app_request, mocked_analytics_module, mocked_sentry_sdk_module):
+    result = OAuthHooks.system_error(app_request, Exception("some exception"))
+
+    assert result.status_code == 302
+    assert result.url == reverse(routes.OAUTH_SYSTEM_ERROR)
+    mocked_analytics_module.error.assert_called_once()
+    mocked_sentry_sdk_module.capture_exception.assert_called_once()
