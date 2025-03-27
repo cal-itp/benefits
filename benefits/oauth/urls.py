ab05e303ff3b969bb845d4edc0395ee98e2e303c
Luis Alvergue <luis@compiler.la>
2025-03-27 16:34:34 +0000

    refactor: use cdt_identity `login` view
    remove Benefit's oauth app login view too since we are now using
    cdt_identity `login`

--- benefits/oauth/urls.py
+++ benefits/oauth/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
+from django.utils.decorators import decorator_from_middleware
 
+from cdt_identity import views as cdt_identity_views
 from benefits.routes import routes
-from . import views
+from . import views, hooks, middleware
 
 
 app_name = "oauth"
 urlpatterns = [
     # /oauth
-    path("login", views.login, name=routes.name(routes.OAUTH_LOGIN)),
+    path(
+        "login",
+        decorator_from_middleware(middleware.FlowUsesClaimsVerificationSessionRequired)(cdt_identity_views.login),
+        {"hooks": hooks.OAuthHooks},
+        name=routes.name(routes.OAUTH_LOGIN),
+    ),
     path("authorize", views.authorize, name=routes.name(routes.OAUTH_AUTHORIZE)),
     path("cancel", views.cancel, name=routes.name(routes.OAUTH_CANCEL)),
     path("logout", views.logout, name=routes.name(routes.OAUTH_LOGOUT)),
