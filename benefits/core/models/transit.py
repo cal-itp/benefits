45292b2e973b7ce1a51b99843d6a3c7afe5d7255
Angela Tran <angela@compiler.la>
2025-04-18 13:40:48 +0000

    feat(model): add new model `LittlepayConfig`
    add fields for environment label, agency label, and API credentials

--- benefits/core/models/transit.py
+++ benefits/core/models/transit.py
@@ -27,6 +27,40 @@ def agency_logo_large(instance, filename):
     return _agency_logo(instance, filename, "lg")
 
 
+class LittlepayConfig(models.Model):
+    """Configuration for connecting to Littlepay, an entity that applies transit agency fare rules to rider transactions."""
+
+    class Environment(models.TextChoices):
+        QA = "qa", "QA"
+        PROD = "prod", "Production"
+
+    id = models.AutoField(primary_key=True)
+    environment = models.TextField(
+        choices=Environment,
+        help_text="A label to indicate which environment this configuration is for.",
+    )
+    agency_slug = models.SlugField(
+        choices=core_context.AgencySlug,
+        help_text="A label to indicate which agency this configuration is for. Note: the field that controls which configuration an agency actually uses is on the TransitAgency model.",  # noqa
+    )
+    audience = models.TextField(
+        help_text="This agency's audience value used to access the TransitProcessor's API.", default="", blank=True
+    )
+    client_id = models.TextField(
+        help_text="This agency's client_id value used to access the TransitProcessor's API.", default="", blank=True
+    )
+    client_secret_name = SecretNameField(
+        help_text="The name of the secret containing this agency's client_secret value used to access the TransitProcessor's API.",  # noqa: E501
+        default="",
+        blank=True,
+    )
+
+    def __str__(self):
+        environment_label = self.Environment(self.environment).label if self.environment else "(unknown)"
+        agency_slug = self.agency_slug if self.agency_slug else "(no agency)"
+        return f"({environment_label}) {agency_slug}"
+
+
 class TransitProcessor(models.Model):
     """An entity that applies transit agency fare rules to rider transactions."""
 
