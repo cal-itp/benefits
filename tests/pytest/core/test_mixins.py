52e93d99bc754c0112f00ba58eb282f83f7b84e9
Kegan Maher <kegan@compiler.la>
2025-05-01 00:23:10 +0000

    feat(views): CBV mixin enforces flow in session
    modeled after existing middleware class FlowSessionRequired,
    and will eventually replace this entirely once all dependent views
    are refactored to CBV

--- tests/pytest/core/test_mixins.py
+++ tests/pytest/core/test_mixins.py
@@ -3,18 +3,16 @@ from django.views import View
 import pytest
 
 from benefits.core.middleware import TEMPLATE_USER_ERROR
-from benefits.core.mixins import AgencySessionRequiredMixin
-
-
-class SampleView(AgencySessionRequiredMixin, View):
-    pass
+from benefits.core.mixins import AgencySessionRequiredMixin, FlowSessionRequiredMixin
 
 
 class TestAgencySessionRequiredMixin:
+    class SampleView(AgencySessionRequiredMixin, View):
+        pass
 
     @pytest.fixture
     def view(self, app_request):
-        v = SampleView()
+        v = self.SampleView()
         v.setup(app_request)
         return v
 
@@ -36,3 +34,32 @@ class TestAgencySessionRequiredMixin:
         view.dispatch(app_request)
 
         assert view.agency == {"agency": "123"}
+
+
+class TestFlowSessionRequiredMixin:
+    class SampleView(FlowSessionRequiredMixin, View):
+        pass
+
+    @pytest.fixture
+    def view(self, app_request):
+        v = self.SampleView()
+        v.setup(app_request)
+        return v
+
+    def test_dispatch_without_flow(self, view, app_request, mocker):
+        mock_session = mocker.patch("benefits.core.mixins.session")
+        mock_session.flow.return_value = False
+
+        response = view.dispatch(app_request)
+
+        assert not hasattr(view, "flow")
+        assert response.status_code == 200
+        assert response.template_name == TEMPLATE_USER_ERROR
+
+    def test_dispatch_with_flow(self, view, app_request, mocker):
+        mock_session = mocker.patch("benefits.core.mixins.session")
+        mock_session.flow.return_value = {"flow": "123"}
+
+        view.dispatch(app_request)
+
+        assert view.flow == {"flow": "123"}
