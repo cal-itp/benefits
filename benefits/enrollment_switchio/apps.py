ba14f3a3bb48206292bb349aa51be88543afd4ac
Angela Tran <angela@compiler.la>
2025-05-13 16:20:28 +0000

    chore: clean up generated files and remove unneeded modules

--- benefits/enrollment_switchio/apps.py
+++ benefits/enrollment_switchio/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class EnrollmentSwitchioConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "enrollment_switchio"
+    name = "benefits.enrollment_switchio"
+    label = "enrollment_switchio"
