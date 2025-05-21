8b563b4cb55249c34cdfbf2b2a6ee1a5bbe7a99b
Angela Tran <angela@compiler.la>
2025-05-21 19:12:24 +0000

    chore: add helper script for manually testing the registration module
    values are expected to be set in your .env file

---
+++ benefits/enrollment_switchio/test.py
@@ -0,0 +1,22 @@
+import os
+
+from benefits.enrollment_switchio import registration
+
+
+def _get_env_values():
+    return dict(
+        api_url=os.environ.get("SWITCHIO_API_URL"),
+        api_key=os.environ.get("SWITCHIO_API_KEY"),
+        api_secret=os.environ.get("SWITCHIO_API_SECRET"),
+        # these paths are relative to the working directory from which you are running this file.
+        private_key=os.path.abspath(os.environ.get("SWITCHIO_PRIVATE_KEY_PATH")),
+        client_certificate_file=os.path.abspath(os.environ.get("SWITCHIO_CLIENT_CERT_PATH")),
+        ca_certificate=os.path.abspath(os.environ.get("SWITCHIO_CA_CERT_PATH")),
+    )
+
+
+if __name__ == "__main__":
+    values = _get_env_values()
+    response = registration.request_registration(**values, eshopResponseMode=registration.EShopResponseMode.FORM_POST)
+
+    print(response)
