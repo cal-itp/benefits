2b455916fbc9694fd4f2606c1fee165e9ec8f6a1
Angela Tran <angela@compiler.la>
2025-05-27 21:38:11 +0000

    refactor: move logic for making API requests into methods on Client

--- benefits/enrollment_switchio/registration.py
+++ benefits/enrollment_switchio/registration.py
@@ -20,21 +20,6 @@ class EShopResponseMode(Enum):
     POST_MESSAGE = "post_message"
 
 
-def _signature_input_string(timestamp, method, request_path, body):
-    return f"{timestamp}{method}{request_path}{body}"
-
-
-def _stp_signature(api_secret, timestamp, method, request_path, body):
-    input_string = _signature_input_string(timestamp, method, request_path, body)
-
-    # must encode inputs for hashing, according to https://stackoverflow.com/a/66958131
-    byte_key = api_secret.encode("utf-8")
-    message = input_string.encode("utf-8")
-    stp_signature = hmac.new(byte_key, message, hashlib.sha256).hexdigest()
-
-    return stp_signature
-
-
 class Client:
 
     def __init__(
@@ -53,6 +38,35 @@ class Client:
         self.client_certificate_file = client_certificate_file
         self.ca_certificate = ca_certificate
 
+    def _signature_input_string(self, timestamp, method, request_path, body):
+        return f"{timestamp}{method}{request_path}{body}"
+
+    def _stp_signature(self, api_secret, timestamp, method, request_path, body):
+        input_string = self._signature_input_string(timestamp, method, request_path, body)
+
+        # must encode inputs for hashing, according to https://stackoverflow.com/a/66958131
+        byte_key = api_secret.encode("utf-8")
+        message = input_string.encode("utf-8")
+        stp_signature = hmac.new(byte_key, message, hashlib.sha256).hexdigest()
+
+        return stp_signature
+
+    def _get_headers(self, method, request_path, request_body: dict):
+        timestamp = str(int(datetime.now().timestamp()))
+
+        return {
+            "Content-Type": "application/json",
+            "STP-APIKEY": self.api_key,
+            "STP-TIMESTAMP": timestamp,
+            "STP-SIGNATURE": self._stp_signature(
+                api_secret=self.api_secret,
+                timestamp=timestamp,
+                method=method,
+                request_path=request_path,
+                body=json.dumps(request_body),
+            ),
+        }
+
     def request_registration(
         self,
         eshopResponseMode: EShopResponseMode,
@@ -65,23 +79,11 @@ class Client:
             "eshopResponseMode": eshopResponseMode.value,
         }
         cert = (self.client_certificate_file, self.private_key)
-        timestamp = str(int(datetime.now().timestamp()))
 
         response = requests.post(
             self.api_url.strip("/") + registration_path,
             json=request_body,
-            headers={
-                "Content-Type": "application/json",
-                "STP-APIKEY": self.api_key,
-                "STP-TIMESTAMP": timestamp,
-                "STP-SIGNATURE": _stp_signature(
-                    api_secret=self.api_secret,
-                    timestamp=timestamp,
-                    method="POST",
-                    request_path=registration_path,
-                    body=json.dumps(request_body),
-                ),
-            },
+            headers=self._get_headers(method="POST", request_path=registration_path, request_body=request_body),
             cert=cert,
             verify=self.ca_certificate,
             timeout=timeout,
