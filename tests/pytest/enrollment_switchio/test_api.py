8b0475074699b7d726b860d768bada9a6b6f0f8d
Angela Tran <angela@compiler.la>
2025-05-29 00:28:09 +0000

    test: add unit test for method that calculates signature
    remove an unnecessary method parameter from Client method. it can use
    the instance variable directly.

--- tests/pytest/enrollment_switchio/test_api.py
+++ tests/pytest/enrollment_switchio/test_api.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import hashlib
+import hmac
 import pytest
 
 from benefits.enrollment_switchio.api import Client
@@ -23,3 +25,20 @@ def test_client_signature_input_string_with_body(client, method, body):
         expected = f"{timestamp}{method}{request_path}{body}"
 
     assert input_string == expected
+
+
+@pytest.mark.parametrize("method", ["GET", "POST"])
+@pytest.mark.parametrize("body", ['{"exampleProperty": "blah"}', None, ""])
+def test_client_stp_signature(client, method, body):
+    timestamp = str(int(datetime.now().timestamp()))
+    request_path = "/api/example"
+
+    stp_signature = client._stp_signature(timestamp=timestamp, method=method, request_path=request_path, body=body)
+
+    # calculate the expected value
+    byte_key = client.api_secret.encode("utf-8")
+    input_string = client._signature_input_string(timestamp=timestamp, method=method, request_path=request_path, body=body)
+    message = input_string.encode("utf-8")
+    expected = hmac.new(byte_key, message, hashlib.sha256).hexdigest()
+
+    assert stp_signature == expected
