39c675681573301d46269eeea536a1702b70ccd4
Angela Tran <angela@compiler.la>
2025-05-31 01:21:55 +0000

    test: update test for `GatewayUrlView.get`

--- tests/pytest/enrollment_switchio/test_views.py
+++ tests/pytest/enrollment_switchio/test_views.py
@@ -1,7 +1,15 @@
 import json
 import pytest
 
+from benefits.enrollment_switchio.api import Registration
 from benefits.enrollment_switchio.views import GatewayUrlView, IndexView
+
+
+@pytest.fixture
+def mocked_api_base_url(mocker):
+    return mocker.patch(
+        "benefits.enrollment_switchio.models.get_secret_by_name", return_value="https://example.com/backend-api"
+    )
 
 
 class TestIndexView:
@@ -41,9 +49,16 @@ class TestGatewayUrlView:
         return v
 
     @pytest.mark.django_db
-    def test_get_gateway_url(self, view, app_request):
+    @pytest.mark.usefixtures("mocked_api_base_url", "mocked_session_agency")
+    def test_get_gateway_url(self, view, app_request, mocker, model_TransitAgency, model_SwitchioConfig):
+        model_TransitAgency.switchio_config = model_SwitchioConfig
+        gateway_url = "https://example.com/cst/?regId=1234"
+        mocker.patch(
+            "benefits.enrollment_switchio.views.Client.request_registration",
+            return_value=Registration(regId="1234", gtwUrl=gateway_url),
+        )
 
         response = view.get(app_request)
 
         assert response.status_code == 200
-        assert json.loads(response.content) == {"gateway_url": "https://server/gateway/uuid"}
+        assert json.loads(response.content) == {"gateway_url": gateway_url}
