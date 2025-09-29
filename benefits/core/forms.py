e4c81efcd84e8b0fba796a4df0e5c1177f22b077
John Gravois <jgravois@compiler.la>
2025-09-29 17:44:20 +0000

    chore: more cleanup

--- benefits/core/forms.py
+++ benefits/core/forms.py
@@ -10,35 +10,16 @@
 # implement transit agency <select/> picker on homepage
 
 # TODO:
-# add translations
 # yank modal/card code for selecting an agency
 # how to sequence updating the logos in production? would we just do that all at once immediately before deploying?
-# ??
 
 from django import forms
-
-from . import models
-
-PLACEHOLDER = "Choose your transit provider"
-
-
-def get_active_agency_names():
-    agency_names = [(None, PLACEHOLDER)]
-    for a in models.TransitAgency.all_active():
-        agency_names.append((a.slug, a.long_name))
-
-    return agency_names
 
 
 class ChooseAgencyForm(forms.Form):
     # this variable name dictates the #id of the associated div
-    select_transit_agency = forms.ChoiceField(
-        choices=get_active_agency_names,
-        label="Enroll today",
-        label_suffix="",
-        required=True,
-        widget=forms.Select(attrs={"class": "form-select", "aria-label": PLACEHOLDER}),
-    )
+    # props are supplied in the FormView (to support localization)
+    select_transit_agency = forms.ChoiceField()
 
     def get_eligibility_url(self):
         return self.cleaned_data["select_transit_agency"] + "/eligibility"
