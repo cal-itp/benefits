bdac07d45f8f2dac9f38cfd7839cb2735e21a3f9
Angela Tran <angela@compiler.la>
2025-10-02 21:22:59 +0000

    refactor(tests): convert unit tests to be for `SuccessView`

--- tests/pytest/enrollment/context/test_flow.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from benefits.core.context import AgencySlug
-from benefits.enrollment import context
-
-
-@pytest.mark.parametrize("slug", AgencySlug)
-def test_enrollent_success(slug):
-    assert context.enrollment_success[slug.value]
