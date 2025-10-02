391e945d09e07a5c8c1e8d28d336719df5632e81
Scott Cranfill <scranfill@compiler.la>
2025-10-02 16:17:30 +0000

    chore: Add reset_db.sh fallback to always run if DB file not found

--- bin/reset_db.sh
+++ bin/reset_db.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 set -ex
 
-# whether to reset database file, defaults to true
-DB_RESET="${DJANGO_DB_RESET:-true}"
+# construct the path to the database file from environment or default
+DB_DIR="${DJANGO_STORAGE_DIR:-.}"
+DB_FILE="${DJANGO_DB_FILE:-django.db}"
+DB_PATH="${DB_DIR}/${DB_FILE}"
+
+if ! [[ -f "{$DB_PATH}" ]]; then
+    # definitely reset if the database file is not found
+    DB_RESET=true
+else
+    # use the DJANGO_DB_RESET env var or default to true
+    DB_RESET="${DJANGO_DB_RESET:-true}"
+fi
 
 if [[ $DB_RESET = true ]]; then
-    # construct the path to the database file from environment or default
-    DB_DIR="${DJANGO_STORAGE_DIR:-.}"
-    DB_FILE="${DJANGO_DB_FILE:-django.db}"
-    DB_PATH="${DB_DIR}/${DB_FILE}"
-
+    # delete existing DB, if present
     rm -f "${DB_PATH}"
 
     # run database migrations and other initialization
