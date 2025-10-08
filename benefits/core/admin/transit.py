6b5b11436d692d332f2797d3671936dbed652b24
Kegan Maher <kegan@compiler.la>
2025-10-08 22:11:26 +0000

    feat(admin): define EligibilityApiConfig ModelAdmin
    clean up field checks on TransitAgency ModelAdmin

--- benefits/core/admin/transit.py
+++ benefits/core/admin/transit.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 from benefits.core import models
-from .mixins import StaffPermissionMixin
+from .mixins import StaffPermissionMixin, SuperuserPermissionMixin
+
+
+@admin.register(models.EligibilityApiConfig)
+class EligibilityApiConfigAdmin(SuperuserPermissionMixin, admin.ModelAdmin):
+    pass
 
 
 @admin.register(models.TransitAgency)
@@ -12,22 +17,8 @@ class TransitAgencyAdmin(StaffPermissionMixin, admin.ModelAdmin):
         if not request.user.is_superuser:
             fields.extend(
                 [
-                    "eligibility_api_private_key",
-                    "eligibility_api_public_key",
                     "sso_domain",
                 ]
             )
 
         return fields or super().get_exclude(request, obj)
-
-    def get_readonly_fields(self, request, obj=None):
-        fields = []
-
-        if not request.user.is_superuser:
-            fields.extend(
-                [
-                    "eligibility_api_id",
-                ]
-            )
-
-        return fields or super().get_readonly_fields(request, obj)
--- tests/pytest/core/admin/test_transit.py
+++ tests/pytest/core/admin/test_transit.py
@@ -2,13 +2,22 @@ import pytest
 from django.contrib import admin
 
 from benefits.core import models
-from benefits.core.admin.transit import TransitAgencyAdmin
-from benefits.core.admin.mixins import StaffPermissionMixin
+from benefits.core.admin.mixins import StaffPermissionMixin, SuperuserPermissionMixin
+from benefits.core.admin.transit import EligibilityApiConfigAdmin, TransitAgencyAdmin
+
+
+@pytest.mark.django_db
+class TestEligibilityApiConfigAdmin:
+    @pytest.fixture(autouse=True)
+    def init(self):
+        self.model_admin = EligibilityApiConfigAdmin(models.EligibilityApiConfig, admin.site)
+
+    def test_permissions_mixin(self):
+        assert isinstance(self.model_admin, SuperuserPermissionMixin)
 
 
 @pytest.mark.django_db
 class TestTransitAgencyAdmin:
-
     @pytest.fixture(autouse=True)
     def init(self):
         self.model_admin = TransitAgencyAdmin(models.TransitAgency, admin.site)
@@ -22,8 +31,6 @@ class TestTransitAgencyAdmin:
             (
                 "staff",
                 [
-                    "eligibility_api_private_key",
-                    "eligibility_api_public_key",
                     "sso_domain",
                 ],
             ),
@@ -43,24 +50,3 @@ class TestTransitAgencyAdmin:
             assert set(excluded) == set(expected)
         else:
             assert excluded is None
-
-    @pytest.mark.parametrize(
-        "user_type,expected",
-        [
-            (
-                "staff",
-                ["eligibility_api_id"],
-            ),
-            ("super", ()),
-        ],
-    )
-    def test_get_readonly_fields(self, admin_user_request, user_type, expected):
-        if expected:
-            model_fields = [f.name for f in self.model_admin.model._meta.get_fields()]
-            assert all(field in model_fields for field in expected)
-
-        request = admin_user_request(user_type)
-
-        readonly = self.model_admin.get_readonly_fields(request)
-
-        assert set(readonly) == set(expected)
