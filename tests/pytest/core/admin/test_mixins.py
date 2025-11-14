be6d24189cd4121621645a24425b010419612995
Kegan Maher <kegan@compiler.la>
2025-11-14 20:59:47 +0000

    feat(admin): permissions mixin for models that are read-only in prod

---
+++ tests/pytest/core/admin/test_mixins.py
@@ -0,0 +1,73 @@
+from django.conf import settings
+
+import pytest
+
+from benefits.core.admin.mixins import ProdReadOnlyPermissionMixin
+
+
+@pytest.mark.django_db
+class TestPermissionsMixins:
+
+    @pytest.mark.parametrize(
+        "MixinClass,runtime_env,user_type,expected",
+        [
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.PROD, "staff", False),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.PROD, "super", False),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.TEST, "staff", True),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.TEST, "super", True),
+        ],
+    )
+    def test_has_add_permission(self, admin_user_request, settings, MixinClass, runtime_env, user_type, expected):
+        settings.RUNTIME_ENVIRONMENT = lambda: runtime_env
+        request = admin_user_request(user_type)
+        mixin = MixinClass()
+
+        assert mixin.has_add_permission(request) == expected
+
+    @pytest.mark.parametrize(
+        "MixinClass,runtime_env,user_type,expected",
+        [
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.PROD, "staff", False),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.PROD, "super", False),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.TEST, "staff", True),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.TEST, "super", True),
+        ],
+    )
+    def test_has_change_permission(self, admin_user_request, settings, MixinClass, runtime_env, user_type, expected):
+        settings.RUNTIME_ENVIRONMENT = lambda: runtime_env
+        request = admin_user_request(user_type)
+        mixin = MixinClass()
+
+        assert mixin.has_change_permission(request) == expected
+
+    @pytest.mark.parametrize(
+        "MixinClass,runtime_env,user_type,expected",
+        [
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.PROD, "staff", False),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.PROD, "super", False),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.TEST, "staff", True),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.TEST, "super", True),
+        ],
+    )
+    def test_has_delete_permission(self, admin_user_request, settings, MixinClass, runtime_env, user_type, expected):
+        settings.RUNTIME_ENVIRONMENT = lambda: runtime_env
+        request = admin_user_request(user_type)
+        mixin = MixinClass()
+
+        assert mixin.has_delete_permission(request) == expected
+
+    @pytest.mark.parametrize(
+        "MixinClass,runtime_env,user_type,expected",
+        [
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.PROD, "staff", True),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.PROD, "super", True),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.TEST, "staff", True),
+            (ProdReadOnlyPermissionMixin, settings.RUNTIME_ENVS.TEST, "super", True),
+        ],
+    )
+    def test_has_view_permission(self, admin_user_request, settings, MixinClass, runtime_env, user_type, expected):
+        settings.RUNTIME_ENVIRONMENT = lambda: runtime_env
+        request = admin_user_request(user_type)
+        mixin = MixinClass()
+
+        assert mixin.has_view_permission(request) == expected
