2fbb51fdfaafbbded36cb9977b0c2845ab147023
Kegan Maher <kegan@compiler.la>
2025-11-18 23:03:58 +0000

    chore(admin): normalize docstrings formatting

--- benefits/core/admin/mixins.py
+++ benefits/core/admin/mixins.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import Group
 
 
 def is_staff_member(user):
-    """Determine if a user is a member of the staff group of Benefits
+    """Determine if a user is a member of the staff group of Benefits.
 
     The staff group of Benefits is also called the 'Cal-ITP' group (defined in settings.STAFF_GROUP_NAME)
     and it is not to be confused with Django's concept of 'staff' which simply means users that can log in to the admin.
@@ -22,8 +22,7 @@ def is_staff_member_or_superuser(user):
 
 
 class ProdReadOnlyPermissionMixin:
-    """
-    A specific mixin for models that should be read-only in Production.
+    """A specific mixin for models that should be read-only in Production.
 
     - Grants `view` to staff/superusers.
     - In Prod: Blocks `add/change/delete` for all users.
@@ -31,9 +30,7 @@ class ProdReadOnlyPermissionMixin:
     """
 
     def _user_can_manage(self, request):
-        """
-        Central logic to check if a user has full management permissions for this model.
-        """
+        """Central logic to check if a user has full management permissions for this model."""
         if settings.RUNTIME_ENVIRONMENT() == settings.RUNTIME_ENVS.PROD:
             return False
         return request.user and is_staff_member_or_superuser(request.user)
@@ -57,14 +54,10 @@ class ProdReadOnlyPermissionMixin:
 
 
 class StaffPermissionMixin:
-    """
-    Grants full `add/change/delete/view` permissions to users who pass the `is_staff_member_or_superuser` check.
-    """
+    """Grants full `add/change/delete/view` permissions to users who pass the `is_staff_member_or_superuser` check."""
 
     def _user_can_manage(self, request):
-        """
-        Central logic to check if a user has full management permissions for this model.
-        """
+        """Central logic to check if a user has full management permissions for this model."""
         return request.user and is_staff_member_or_superuser(request.user)
 
     def has_add_permission(self, request):
@@ -84,14 +77,10 @@ class StaffPermissionMixin:
 
 
 class SuperuserPermissionMixin:
-    """
-    Grants `add/change/delete/view` permissions to superusers only.
-    """
+    """Grants `add/change/delete/view` permissions to superusers only."""
 
     def _user_can_manage(self, request):
-        """
-        Central logic to check if a user has full management permissions for this model.
-        """
+        """Central logic to check if a user has full management permissions for this model."""
         return request.user and request.user.is_superuser
 
     def has_add_permission(self, request):
