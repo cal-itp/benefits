477de6ce8cbbbcb86ee8eab54f6dd4e46b1b37dc
Kegan Maher <kegan@compiler.la>
2026-01-12 21:21:51 +0000

    chore(terraform): format existing files

--- terraform/email.tf
+++ terraform/email.tf
@@ -7,7 +7,7 @@ locals {
   # Determine the correct sender domain based on the environment.
   # If is_prod is true, use the custom domain; otherwise, use the Azure-managed domain.
   sender_domain = local.is_prod ? azurerm_email_communication_service_domain.custom[0].mail_from_sender_domain : azurerm_email_communication_service_domain.azure_managed[0].mail_from_sender_domain
-  sender_email = "DoNotReply@${local.sender_domain}"
+  sender_email  = "DoNotReply@${local.sender_domain}"
 }
 
 resource "azurerm_key_vault_secret" "azure_communication_connection_string" {
