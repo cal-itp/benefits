88ea7fc215e9b7e8b2143858df08ab94253d0ce5
Kegan Maher <kegan@compiler.la>
2026-01-28 01:25:53 +0000

    feat(agencies): add Roseville slug

--- benefits/core/context/agency.py
+++ benefits/core/context/agency.py
@@ -11,6 +11,7 @@ class AgencySlug(models.TextChoices):
     EDCTA = "edcta", "edcta"
     NEVCO = "nevco", "nevco"
     RABA = "raba", "raba"
+    ROSEVILLE = "roseville", "roseville"
     SACRT = "sacrt", "sacrt"
     SBMTD = "sbmtd", "sbmtd"
     SLORTA = "slorta", "slorta"
