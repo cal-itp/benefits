8cd8a1f6743dbaacf89bfe0a5fbe7e64c611dcce
Angela Tran <angela@compiler.la>
2026-02-11 20:58:07 +0000

    test: remove testing of Eligibility Start dictionary usage / conversion
    I don't know that these tests are all that useful, especially if it is that
    we may end up replacing them with simpler, variablized copy sooner than later.

--- tests/pytest/eligibility/context/test_flow.py
+++ tests/pytest/eligibility/context/test_flow.py
@@ -2,27 +2,7 @@ import pytest
 
 from benefits.core import context as core_context
 from benefits.eligibility import context as eligibility_context
-from benefits.eligibility.context.flow import CTAButton, EligibilityStart, EligibilityUnverified
-from benefits.routes import routes
-
-
-@pytest.mark.parametrize("system_name", core_context.SystemName)
-def test_eligibility_start(system_name):
-    assert eligibility_context.eligibility_start[system_name.value]
-
-
-def test_EligibilityStart_dict():
-    context_object = EligibilityStart(
-        page_title="Test page title",
-        headline_text="This is a headline.",
-        eligibility_item_headline="Item headline",
-        eligibility_item_body="Item body",
-        call_to_action_button=CTAButton(text="button text", route=routes.INDEX),
-    )
-
-    context_dict = context_object.dict()
-
-    assert isinstance(context_dict, dict)
+from benefits.eligibility.context.flow import EligibilityUnverified
 
 
 @pytest.mark.parametrize(
