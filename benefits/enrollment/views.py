3e66e34fd106da87af83ea2fa64e42c0ad6ce83f
Kegan Maher <kegan@compiler.la>
2026-02-13 19:17:51 +0000

    refactor: move formatted_gettext_lazy helper to context_processors

--- benefits/enrollment/views.py
+++ benefits/enrollment/views.py
@@ -11,8 +11,8 @@ from django.urls import reverse
 from django.views.generic import RedirectView, TemplateView
 
 from benefits.core import models, session
-from benefits.core.context import formatted_gettext_lazy as _
 from benefits.core.context.flow import SystemName
+from benefits.core.context_processors import formatted_gettext_lazy as _
 from benefits.core.mixins import (
     AgencySessionRequiredMixin,
     EligibleSessionRequiredMixin,
