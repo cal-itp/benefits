9e5f37abd669695588984de41b76fb81e4e7ea87
Kegan Maher <kegan@compiler.la>
2026-02-18 20:57:05 +0000

    ci(onboarding): optional input for estimated launch date

--- .github/workflows/agency-onboarding/create-child-issue.js
+++ .github/workflows/agency-onboarding/create-child-issue.js
@@ -9,7 +9,7 @@ module.exports = async ({
   templateName,
   title,
 }) => {
-  const { long_name, short_name, transit_processor, website } =
+  const { long_name, short_name, transit_processor, website, launch_date } =
     context.payload.inputs;
 
   // read and process body template
@@ -25,7 +25,8 @@ module.exports = async ({
     .replace(/{{LONG_NAME}}/g, long_name)
     .replace(/{{SHORT_NAME}}/g, short_name)
     .replace(/{{TRANSIT_PROCESSOR}}/g, transit_processor)
-    .replace(/{{WEBSITE}}/g, website || "N/A");
+    .replace(/{{WEBSITE}}/g, website || "N/A")
+    .replace(/{{LAUNCH_DATE}}/g, launch_date || "TBD");
 
   // create the child issue
   const child = await github.rest.issues.create({
--- .github/workflows/agency-onboarding/parent.md
+++ .github/workflows/agency-onboarding/parent.md
@@ -4,5 +4,6 @@
 - **Short Name:** {{SHORT_NAME}}
 - **Transit Processor:** {{TRANSIT_PROCESSOR}}
 - **Website:** [{{WEBSITE}}]({{WEBSITE}})
+- **Estimated Launch Date:** {{LAUNCH_DATE}}
 
 _This issue serves as the parent epic for the onboarding tasks._
