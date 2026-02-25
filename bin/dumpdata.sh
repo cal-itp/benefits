c11a20dc0bd4ff0282321782d080f85ef35423f8
Angela Tran <angela@compiler.la>
2026-02-25 23:24:22 +0000

    feat: add prompts for input needed and run other scripts where needed

--- bin/dumpdata.sh
+++ bin/dumpdata.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
 
-python manage.py dumpdata --exclude auth.permission --exclude contenttypes > unreviewed_fixtures.json
+# Let the user know what fixtures they're pointed at currently.
+echo -e "Using fixtures from: $DJANGO_DB_FIXTURES \n"
+
+# Prompt the user to checkout the commit where those fixtures work.
+read -p "Enter the commit on main just prior to model changes: " pre_model_change_commit
+
+# Checkout that commit and reset the database.
+git checkout $pre_model_change_commit
+./bin/reset_db.sh
+
+# Prompt the user to checkout the commit with the new model changes.
+read -p "Enter the commit with the new model changes: " post_model_change_commit
+
+# Checkout that commit and run migrations.
+git checkout $post_model_change_commit
+./bin/init.sh
+
+# Export migrated fixtures and prompt user to review migrated contents.
+temp_file_name="unreviewed_fixtures.json"
+python manage.py dumpdata --exclude auth.permission --exclude contenttypes > $temp_file_name
+echo "Review the migrated fixtures at ./$temp_file_name"
