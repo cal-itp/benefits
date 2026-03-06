07db0fe0bcc6209d7d6bf3c35282d371b2865dfb
Luis Alvergue <luis@compiler.la>
2026-03-06 14:53:40 +0000

    feat(infra): add virtual network and subnets for PostgreSQL server
    since PostgreSQL Flexible Server requires a delegated subnet, we
    add a virtual network and two subnets: one for the App Service and
    one for PostgreSQL server. Further configuration of the subnets will
    be done in future commits.

--- terraform/network.tf
+++ terraform/network.tf
@@ -6,4 +6,35 @@ locals {
 
   # use the subnet ID rather than referencing it as a data source to work around permissions issues
   subnet_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.network_resource_group_name}/providers/Microsoft.Network/virtualNetworks/${local.vnet_name}/subnets/${local.subnet_name}"
+
+  subnet_prefix = "SNET-CDT-PUB-VIP-CALITP-${local.env_letter}"
+  network_subnets = {
+    "APP" = ["10.0.0.0/26"]  # 64 addresses - 10.0.4.0 to 10.0.4.63
+    "DB"  = ["10.0.0.64/26"] # 64 addresses - 10.0.0.64 to 10.0.0.127
+  }
+}
+
+# The primary VNet (per environment)
+resource "azurerm_virtual_network" "main" {
+  name                = "VNET-CDT-PUB-VIP-CALITP-${local.env_letter}-001"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/24"] # 256 addresses - 10.0.0.0 to 10.0.0.255
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+# The subnets for the primary VNet
+resource "azurerm_subnet" "main" {
+  # This for_each loop creates a subnet for each item in the network_subnets variable.
+  for_each = local.network_subnets
+
+  name                 = "${local.subnet_prefix}-${each.key}"
+  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = data.azurerm_resource_group.main.name
+  address_prefixes     = each.value
+
+  default_outbound_access_enabled = false
 }
