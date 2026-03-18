4af400d8d2b7f3455f8f6c1d6b6625092aeb8f59
Luis Alvergue <luis@compiler.la>
2026-03-18 22:15:18 +0000

    chore(infra): remove reference to VNet managed by CDT

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,12 +1,4 @@
 locals {
-  # VNet uses a shared Resource Group, different from App Service Resource Group we use for Benefits stuff
-  network_resource_group_name = "RG-CDT-PUB-SHRD-W-P-001"
-  vnet_name                   = "VNET-CDT-PUB-SHRD-W-P-001"
-  subnet_name                 = local.is_prod ? "SNET-CDT-PUB-CALITP-P-001" : "SN-CDT-PUB-CALITP-${local.env_letter}-001"
-
-  # use the subnet ID rather than referencing it as a data source to work around permissions issues
-  subnet_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.network_resource_group_name}/providers/Microsoft.Network/virtualNetworks/${local.vnet_name}/subnets/${local.subnet_name}"
-
   subnet_prefix = "SNET-CDT-PUB-VIP-CALITP-${local.env_letter}"
   network_subnets = {
     "APP" = {
