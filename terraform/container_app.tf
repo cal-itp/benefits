d89588f7b7ee1f8c2714294ee9e64f53f9d31dd9
Luis Alvergue <luis@compiler.la>
2026-04-03 16:57:42 +0000

    chore(infra): add environment variables for Container App

--- terraform/container_app.tf
+++ terraform/container_app.tf
@@ -52,6 +52,140 @@ resource "azurerm_container_app" "main" {
       image  = "${var.CONTAINER_REGISTRY}/${var.CONTAINER_REPOSITORY}:${var.CONTAINER_TAG}"
       cpu    = 0.5
       memory = "1Gi"
+
+      # Amplitude
+      env {
+        name  = "ANALYTICS_KEY"
+        value = local.is_dev ? null : "${local.secret_prefix}analytics-key)"
+      }
+
+      # Requests
+      env {
+        name  = "REQUESTS_CONNECT_TIMEOUT"
+        value = "${local.secret_prefix}requests-connect-timeout)"
+      }
+      env {
+        name  = "REQUESTS_READ_TIMEOUT"
+        value = "${local.secret_prefix}requests-read-timeout)"
+      }
+
+      # Django Azure Email Backend
+      env {
+        name  = "AZURE_COMMUNICATION_CONNECTION_STRING"
+        value = "${local.secret_prefix}${local.azure_communication_connection_string_name})"
+      }
+      env {
+        name  = "DEFAULT_FROM_EMAIL"
+        value = local.sender_email
+      }
+
+      # Django settings
+      env {
+        name  = "DJANGO_ALLOWED_HOSTS"
+        value = "${local.secret_prefix}django-allowed-hosts)"
+      }
+      env {
+        name  = "DJANGO_STORAGE_DIR"
+        value = "${local.secret_prefix}django-storage-dir)"
+      }
+      env {
+        name  = "DJANGO_DEBUG"
+        value = local.is_prod ? null : "${local.secret_prefix}django-debug)"
+      }
+      env {
+        name  = "DJANGO_LOG_LEVEL"
+        value = "${local.secret_prefix}django-log-level)"
+      }
+      env {
+        name  = "DJANGO_RECAPTCHA_SECRET_KEY"
+        value = "${local.secret_prefix}django-recaptcha-secret-key)"
+      }
+      env {
+        name  = "DJANGO_RECAPTCHA_SITE_KEY"
+        value = "${local.secret_prefix}django-recaptcha-site-key)"
+      }
+      env {
+        name  = "DJANGO_SECRET_KEY"
+        value = "${local.secret_prefix}django-secret-key)"
+      }
+      env {
+        name  = "DJANGO_TRUSTED_ORIGINS"
+        value = "${local.secret_prefix}django-trusted-origins)"
+      }
+      env {
+        name  = "DJANGO_DB_NAME"
+        value = "${local.secret_prefix}django-db-name)"
+      }
+      env {
+        name  = "DJANGO_DB_USER"
+        value = "${local.secret_prefix}django-db-user)"
+      }
+      env {
+        name  = "DJANGO_DB_PASSWORD"
+        value = "${local.secret_prefix}${local.django_db_password_secret_name})"
+      }
+
+      # Database settings
+      env {
+        name  = "USE_POSTGRES"
+        value = "${local.secret_prefix}use-postgres)"
+      }
+      env {
+        name  = "POSTGRES_HOSTNAME"
+        value = azurerm_postgresql_flexible_server.main.fqdn
+      }
+      env {
+        name  = "HEALTHCHECK_USER_AGENTS"
+        value = local.is_dev ? null : "${local.secret_prefix}healthcheck-user-agents)"
+      }
+
+      # Google SSO for Admin
+      env {
+        name  = "GOOGLE_SSO_CLIENT_ID"
+        value = "${local.secret_prefix}google-sso-client-id)"
+      }
+      env {
+        name  = "GOOGLE_SSO_PROJECT_ID"
+        value = "${local.secret_prefix}google-sso-project-id)"
+      }
+      env {
+        name  = "GOOGLE_SSO_CLIENT_SECRET"
+        value = "${local.secret_prefix}google-sso-client-secret)"
+      }
+      env {
+        name  = "GOOGLE_SSO_ALLOWABLE_DOMAINS"
+        value = "${local.secret_prefix}google-sso-allowable-domains)"
+      }
+      env {
+        name  = "GOOGLE_SSO_STAFF_LIST"
+        value = "${local.secret_prefix}google-sso-staff-list)"
+      }
+      env {
+        name  = "GOOGLE_SSO_SUPERUSER_LIST"
+        value = "${local.secret_prefix}google-sso-superuser-list)"
+      }
+      env {
+        name  = "SSO_SHOW_FORM_ON_ADMIN_PAGE"
+        value = "${local.secret_prefix}sso-show-form-on-admin-page)"
+      }
+
+      # Sentry
+      env {
+        name  = "SENTRY_DSN"
+        value = "${local.secret_prefix}sentry-dsn)"
+      }
+      env {
+        name  = "SENTRY_ENVIRONMENT"
+        value = local.env_name
+      }
+      env {
+        name  = "SENTRY_REPORT_URI"
+        value = "${local.secret_prefix}sentry-report-uri)"
+      }
+      env {
+        name  = "SENTRY_TRACES_SAMPLE_RATE"
+        value = "${local.secret_prefix}sentry-traces-sample-rate)"
+      }
     }
   }
 
