4ccdfc4a577b97540ea40c80ae7c1cfb380631cf
John Gravois <jgravois@compiler.la>
2026-05-04 17:50:36 +0000

    fix: use OS agnostic UTC flag

--- terraform/restart-app.sh
+++ terraform/restart-app.sh
@@ -10,6 +10,7 @@ fi
 ENV="$1"
 APP="AS-CDT-PUB-VIP-CALITP-$ENV-001"
 RG="RG-CDT-PUB-VIP-CALITP-$ENV-001"
-NOW=$(date --utc)
+# -u is a terse equivalent to --utc that is also recognized by MacOS
+NOW=$(date -u)
 
 az webapp config appsettings set --name "$APP" --resource-group "$RG" --settings change_me_to_refresh_secrets="$NOW"
