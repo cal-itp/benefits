41d6908a57fbb26802db8aadc6a48b42a4ee7aa3
John Gravois <jgravois@compiler.la>
2026-07-31 21:43:58 +0000

    refactor: flag more sensitive terraform values

--- terraform/modules/application/outputs.tf
+++ terraform/modules/application/outputs.tf
@@ -1,9 +1,11 @@
 output "web_principal_id" {
   description = "The Principal ID of the Benefits (web) Container App Managed Identity"
   value       = azurerm_container_app.web.identity[0].principal_id
+  sensitive   = true
 }
 
 output "pgadmin_principal_id" {
   description = "The Principal ID of the pgAdmin Container App Managed Identity"
   value       = azurerm_container_app.pgadmin.identity[0].principal_id
+  sensitive   = true
 }
