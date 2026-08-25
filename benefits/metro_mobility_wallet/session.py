d7dfc1c6e83300ae53322b89df0c4551a768bc79
Angela Tran <angela@compiler.la>
2026-08-25 19:38:22 +0000

    feat(metro/session): add HttpRequest to be stored in Session

--- benefits/metro_mobility_wallet/session.py
+++ benefits/metro_mobility_wallet/session.py
@@ -1,8 +1,13 @@
 import logging
 
+from django.http import HttpRequest
+
 logger = logging.getLogger(__name__)
 
 
 class Session:
-    def __init__(self):
-        pass
+    def __init__(self, request: HttpRequest):
+        """Initialize a new Metro Mobility Wallet session wrapper for this request"""
+
+        self.request = request
+        self.session = request.session
--- tests/pytest/metro_mobility_wallet/test_session.py
+++ tests/pytest/metro_mobility_wallet/test_session.py
@@ -1,7 +1,17 @@
+import pytest
+from django.http import HttpRequest
+
 from benefits.metro_mobility_wallet.session import Session
 
 
 class TestSession:
-    def test_init(self):
-        session = Session()
-        assert session
+    @pytest.fixture
+    def mock_request(self, mocker):
+        request = mocker.MagicMock(spec=HttpRequest)
+        request.session = {"session_id": 123}
+        return request
+
+    def test_init(self, mock_request):
+        session = Session(mock_request)
+        assert session.request == mock_request
+        assert session.session == mock_request.session
