c3bc17462f958f03c56b16d40e736db4edcd3f1e
John Gravois <jgravois@compiler.la>
2026-09-04 15:59:25 +0000

    chore: define an initial InitConfig model

--- benefits/enrollment_init/admin.py
+++ benefits/enrollment_init/admin.py
@@ -1,3 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from benefits.core.admin.mixins import StaffPermissionMixin
+from benefits.enrollment_init import models
+
+
+@admin.register(models.InitConfig)
+class InitConfigAdmin(StaffPermissionMixin, admin.ModelAdmin):
+    pass
--- benefits/enrollment_init/models.py
+++ benefits/enrollment_init/models.py
@@ -1,3 +1,50 @@
-# from django.db import models
+from django.core.exceptions import ValidationError
+from django.db import models
 
-# Create your models here.
+from benefits.core.models import SecretNameField, TransitProcessorConfig
+
+
+class InitConfig(TransitProcessorConfig):
+    """Configuration for connecting to INIT, an entity that applies transit agency fare rules to rider transactions."""
+
+    tokenization_api_key = models.CharField(help_text="The Collect.js API key used for tokenization.", default="", blank=True)
+
+    registration_base_url = models.URLField(
+        help_text="The absolute base url of the MOBILEvario API instance.", default="", blank=True
+    )
+
+    registration_username = models.CharField(
+        help_text="The username used to authenticate with MOBILEvario.",
+        default="",
+        blank=True,
+        max_length=50,
+    )
+
+    registration_password_secret_name = SecretNameField(
+        help_text="The name of the secret containing the password used to authenticate with MOBILEvario, "
+        "typically: [agency]-init-registration-password",
+        default="",
+        blank=True,
+        max_length=50,
+    )
+
+    @property
+    def registration_password(self):
+        secret_field = self._meta.get_field("registration_password_secret_name")
+        return secret_field.secret_value(self)
+
+    def clean(self):
+        field_errors = {}
+
+        if self.pk and self.transitagency_set and any([agency.active for agency in self.transitagency_set.all()]):
+            message = "This field is required when this configuration is referenced by an active transit agency."
+            needed = dict(
+                tokenization_api_key=self.tokenization_api_key,
+            )
+            field_errors.update({k: ValidationError(message) for k, v in needed.items() if not v})
+
+        if field_errors:
+            raise ValidationError(field_errors)
+
+    class Meta:
+        verbose_name = "INIT Config"
