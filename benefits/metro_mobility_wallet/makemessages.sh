51a88c3f1c0ac16cd8dc8eb0ed02a9ed27d0cf78
Luis Alvergue <luis@compiler.la>
2026-09-10 15:03:05 +0000

    feat(i18n): create first time message files for MMW-specific languages

---
+++ benefits/metro_mobility_wallet/makemessages.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eu
+
+# To only process message files in metro_mobility_wallet, set working directory to the folder containing this script
+# https://docs.djangoproject.com/en/6.1/ref/django-admin/#makemessages
+cd "$(dirname "$0")"
+
+# generate .PO files from msgids in template and view files
+python ../../manage.py makemessages -l en -l es -l "zh_Hans" -l "zh_Hant" -l ko -l ja -l th -l vi -l ru -l hy  --no-obsolete --no-location
+
+# Apply bug-report link fix strictly to metro_mobility_wallet's locale files
+sed -i 's/Report-Msgid-Bugs-To\:/Report-Msgid-Bugs-To\: https\:\/\/github.com\/cal-itp\/benefits\/issues/g' locale/*/*/*.po
