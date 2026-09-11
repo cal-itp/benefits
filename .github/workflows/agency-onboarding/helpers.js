4f71da0a7c63baea31600ff32080b65cf180a1c5
Kegan Maher <kegan@compiler.la>
2026-09-11 21:38:13 +0000

    ci(onboarding): link adoption table update PR to issue

--- .github/workflows/agency-onboarding/helpers.js
+++ .github/workflows/agency-onboarding/helpers.js
@@ -134,7 +134,7 @@ function convertToMMYYYY(dateStr) {
   return dateStr.trim();
 }
 
-export const updateAdoptionTable = async ({ github, context, parentIssue }) => {
+export const updateAdoptionTable = async ({ github, context, issueNumber }) => {
   const { agency_name, launch_date, agency_dba } = context.payload.inputs;
 
   const launchDateStr = convertToMMYYYY(launch_date);
@@ -225,7 +225,7 @@ export const updateAdoptionTable = async ({ github, context, parentIssue }) => {
     title: `docs: adds ${agency_name} to adoption table`,
     head: branchName,
     base: "main",
-    body: `Adds **${agency_name}** to the adoption table in the docs.\n\nPart of onboarding epic #${parentIssue}.`,
+    body: `Closes #${issueNumber}\n\nAdds **${agency_name}** to the adoption table in the docs.`,
     draft: true,
   });
 
