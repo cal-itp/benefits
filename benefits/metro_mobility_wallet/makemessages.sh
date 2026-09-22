0a8a8394d68042c0465866507449e1480a285322
Luis Alvergue <luis@compiler.la>
2026-09-22 14:02:45 +0000

    fix(i18n): use django-admin instead of python manage.py
    to make message files. The difference is that django-admin enforces
    processing message files only for the Django app from which the
    command is ran, giving us a clean separation between Benefits and
    MMW. For more details see the Django docs
    /topics/i18n/translation/#message-files
    
    and clean up Report-Msgid-Bugs-To field

--- benefits/metro_mobility_wallet/makemessages.sh
+++ benefits/metro_mobility_wallet/makemessages.sh
@@ -5,8 +5,10 @@ set -eu
 # https://docs.djangoproject.com/en/6.1/ref/django-admin/#makemessages
 cd "$(dirname "$0")"
 
-# generate .PO files from msgids in template and view files
-python ../../manage.py makemessages -a  --no-obsolete --no-location
+# Generate .PO files from msgids in template and view files
+# Note that django-admin is used to enforce discovering only the MMW locale
+# https://docs.djangoproject.com/en/6.1/topics/i18n/translation/#message-files
+django-admin makemessages -a  --no-obsolete --no-location
 
 # Apply bug-report link fix strictly to metro_mobility_wallet's locale files
 sed -i 's/Report-Msgid-Bugs-To\:/Report-Msgid-Bugs-To\: https\:\/\/github.com\/cal-itp\/benefits\/issues/g' locale/*/*/*.po
