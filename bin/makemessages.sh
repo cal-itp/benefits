d90f68daad45e345efc7a83c73607caa50e808d0
Luis Alvergue <luis@compiler.la>
2026-09-23 15:34:34 +0000

    refactor(i18n): process message files for Benefits and MMW
    but just using the one script in bin/makemessages.sh
    Note that the script independently processes Benefits and MMW.

--- bin/makemessages.sh
+++ bin/makemessages.sh
@@ -3,8 +3,19 @@ set -eu
 
 # generate initial .PO files from msgids in template and view files
 
-python manage.py makemessages -a --no-obsolete --no-location
+# process Benefits using the -i flag to ignore the MMW directory
+# this ensures strings from MMW do not leak into Benefits' message files
+python manage.py makemessages -a --no-obsolete --no-location --ignore="benefits/metro_mobility_wallet/*"
+# to enforce only processing MMW messages navigate into the app directory and use django-admin makemessages
+# Django restricts its scan to this folder and outputs the translations directly to benefits/metro_mobility_wallet/locale/
+# https://docs.djangoproject.com/en/6.1/topics/i18n/translation/#message-files
+cd benefits/metro_mobility_wallet
+django-admin makemessages -a  --no-obsolete --no-location
+cd ../..
 
 # put back bug-report link
 
+# for Benefits
 sed -i 's/Report-Msgid-Bugs-To\:/Report-Msgid-Bugs-To\: https\:\/\/github.com\/cal-itp\/benefits\/issues/g' benefits/locale/*/*/*.po
+# for MMW
+sed -i 's/Report-Msgid-Bugs-To\:/Report-Msgid-Bugs-To\: https\:\/\/github.com\/cal-itp\/benefits\/issues/g' benefits/metro_mobility_wallet/locale/*/*/*.po
