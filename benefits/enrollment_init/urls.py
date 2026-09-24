966637d769c8d4a13b05a9a95559711fcf379e50
John Gravois <jgravois@compiler.la>
2026-09-24 13:39:41 +0000

    feat: wire up enrollment_init landing page

---
+++ benefits/enrollment_init/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from benefits.enrollment_init.views import IndexView
+
+from .routes import routes
+
+app_name = "init"
+urlpatterns = [
+    # /init/
+    path("", IndexView.as_view(), name=routes.name(routes.ENROLLMENT_INIT_INDEX)),
+]
